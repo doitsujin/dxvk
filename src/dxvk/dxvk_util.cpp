aaffc8e26f2d0b3522ba2d42cc464188fc87d1a4
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2018-01-21 00:04:17 +0100

    [dxvk] Fixed incorrect image packing behaviour

--- src/dxvk/dxvk_util.cpp
+++ src/dxvk/dxvk_util.cpp
@@ -48,8 +48,8 @@ namespace dxvk::util {
     const VkDeviceSize bytesPerLayer = blockCount.height * bytesPerRow;
     const VkDeviceSize bytesTotal    = blockCount.depth  * bytesPerLayer;
     
-    const bool directCopy = (bytesPerRow   == pitchPerRow  ) || (blockCount.height == 1)
-                         && (bytesPerLayer == pitchPerLayer) || (blockCount.depth  == 1);
+    const bool directCopy = ((bytesPerRow   == pitchPerRow  ) || (blockCount.height == 1))
+                         && ((bytesPerLayer == pitchPerLayer) || (blockCount.depth  == 1));
     
     if (directCopy) {
       std::memcpy(dstData, srcData, bytesTotal);
