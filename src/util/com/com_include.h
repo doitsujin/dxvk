569b6fc6da9a732d4cacbfe32a7d46822420bb7a
Mikhail Paulyshka <me@mixaill.tk>
2018-03-03 15:57:56 +0300

    [utils] fixes for MSVC
    * __mingw_uuidof() does not exists in MSVC
    * apply GCC pragma only for GCC
    * added missing header

--- src/util/com/com_include.h
+++ src/util/com/com_include.h
@@ -2,7 +2,9 @@
 
 // GCC complains about the COM interfaces
 // not having virtual destructors
+#ifdef __GNUC__
 #pragma GCC diagnostic ignored "-Wnon-virtual-dtor"
+#endif // __GNUC__
 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
