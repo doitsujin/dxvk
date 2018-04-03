c9465b39d1917c43aa79d5787631412a53ead3d7
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2018-04-03 11:56:02 +0200

    [util] Implement spin lock

--- src/dxvk/dxvk_include.h
+++ src/dxvk/dxvk_include.h
@@ -12,6 +12,8 @@
 #include "../util/rc/util_rc.h"
 #include "../util/rc/util_rc_ptr.h"
 
+#include "../util/sync/sync_spinlock.h"
+
 #include "./vulkan/dxvk_vulkan_extensions.h"
 #include "./vulkan/dxvk_vulkan_loader.h"
 #include "./vulkan/dxvk_vulkan_names.h"
---
+++ src/util/sync/sync_spinlock.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <atomic>
+#include <thread>
+
+namespace dxvk::sync {
+  
+  /**
+   * \brief Spin lock
+   * 
+   * A low-overhead spin lock which can be used to
+   * protect data structures for a short duration
+   * in case the structure is not likely contested.
+   */
+  class Spinlock {
+    
+  public:
+    
+    Spinlock() { }
+    ~Spinlock() { }
+    
+    Spinlock             (const Spinlock&) = delete;
+    Spinlock& operator = (const Spinlock&) = delete;
+    
+    void lock() {
+      while (!this->try_lock())
+        std::this_thread::yield();
+    }
+    
+    void unlock() {
+      m_lock.store(0, std::memory_order_release);
+    }
+    
+    bool try_lock() {
+      uint32_t expected = 0;
+      return m_lock.compare_exchange_strong(expected, 1,
+        std::memory_order_acquire,
+        std::memory_order_relaxed);
+    }
+    
+  private:
+    
+    std::atomic<uint32_t> m_lock = { 0 };
+    
+  };
+  
+}
