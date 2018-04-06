d90d697459e0b24fb5c58022d24cf6f0381b311c
pchome <pchome@users.noreply.github.com>
2018-04-06 18:22:20 +0300

    [util] Define typeof keyword for WINE guiddef.h
    GCC: `-std options disable certain keywords`
    https://gcc.gnu.org/onlinedocs/gcc/Alternate-Keywords.html
    
    MinGW: `#define __uuidof(type) __mingw_uuidof<__typeof(type)>()`
    WINE: `#define __uuidof(type) __wine_uuidof<typeof(type)>()`

--- src/util/com/com_include.h
+++ src/util/com/com_include.h
@@ -9,3 +9,9 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <unknwn.h>
+
+// GCC: -std options disable certain keywords
+// https://gcc.gnu.org/onlinedocs/gcc/Alternate-Keywords.html
+#if defined(__WINE__) && !defined(typeof)
+#define typeof __typeof
+#endif
