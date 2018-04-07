62b99c9bd2395b9219aefd06e01f1082792a6ca6
pchome <pchome@users.noreply.github.com>
2018-04-07 15:01:13 +0300

    [util] Fix compilation with WINE headers  (#246)
    * [util] Define typeof keyword for WINE guiddef.h
    
    GCC: `-std options disable certain keywords`
    https://gcc.gnu.org/onlinedocs/gcc/Alternate-Keywords.html
    
    MinGW: `#define __uuidof(type) __mingw_uuidof<__typeof(type)>()`
    WINE: `#define __uuidof(type) __wine_uuidof<typeof(type)>()`
    
    * [util] Fix compilation with WINE headers
    
    Overall definitions are the same but `__wine_` prefix used in .
    
    MinGW: _mingw.h + guiddef.h
    WINE: guiddef.h

--- src/util/com/com_guid.h
+++ src/util/com/com_guid.h
@@ -6,8 +6,13 @@
 #include "com_include.h"
 
 #ifndef _MSC_VER
-#define DXVK_DEFINE_GUID(iface) \
-  template<> inline GUID const& __mingw_uuidof<iface> () { return iface::guid; }
+# ifdef __WINE__
+#   define DXVK_DEFINE_GUID(iface) \
+      template<> inline GUID const& __wine_uuidof<iface> () { return iface::guid; }
+# else
+#   define DXVK_DEFINE_GUID(iface) \
+      template<> inline GUID const& __mingw_uuidof<iface> () { return iface::guid; }
+# endif
 #endif
 
 std::ostream& operator << (std::ostream& os, REFIID guid);
