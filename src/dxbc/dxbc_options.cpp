1b6442b859100b9c4297bba325437eeb10380056
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2018-04-11 00:20:24 +0200

    [dxbc] Disable Nvidia-specific workarounds for 396.18 and above

--- src/dxbc/dxbc_options.cpp
+++ src/dxbc/dxbc_options.cpp
@@ -9,7 +9,8 @@ namespace dxvk {
     // Apply driver-specific workarounds
     const DxvkGpuVendor vendor = static_cast<DxvkGpuVendor>(devProps.vendorID);
     
-    if (vendor == DxvkGpuVendor::Nvidia) {
+    if (vendor == DxvkGpuVendor::Nvidia
+     && devProps.driverVersion <= VK_MAKE_VERSION(396, 18, 0)) {
       // Older versions of the driver expect the
       // coordinate vector to have an extra component
       this->addExtraDrefCoordComponent = true;
