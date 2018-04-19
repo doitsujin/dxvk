c2854e1fb9db4f48b9069e8362518fd42a137199
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2018-04-19 20:13:53 +0200

    [dxgi] Added IDXGIVkInteropSurface interface

--- src/dxgi/dxgi_interfaces.h
+++ src/dxgi/dxgi_interfaces.h
@@ -135,6 +135,49 @@ IDXGIVkPresenter : public IUnknown {
 
 
 /**
+ * \brief DXGI surface interface for Vulkan interop
+ * 
+ * Provides access to the backing resource of a
+ * DXGI surface, which is typically a D3D texture.
+ */
+MIDL_INTERFACE("5546cf8c-77e7-4341-b05d-8d4d5000e77d")
+IDXGIVkInteropSurface : public IUnknown {
+  static const GUID guid;
+  
+  /**
+   * \brief Retrieves Vulkan image info
+   * 
+   * Retrieves both the image handle as well as the image's
+   * properties. Any of the given pointers may be \c nullptr.
+   * 
+   * If \c pInfo is not \c nullptr, the following rules apply:
+   * - \c pInfo->sType \e must be \c VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO
+   * - \c pInfo->pNext \e must be \c nullptr or point ti a supported
+   *   extension-specific structure (currently none)
+   * - \c pInfo->queueFamilyIndexCount must be the length of the
+   *   \c pInfo->pQueueFamilyIndices array, in \c uint32_t units.
+   * - \c pInfo->pQueueFamilyIndices must point to a pre-allocated
+   *   array of \c uint32_t of size \c pInfo->pQueueFamilyIndices.
+   * 
+   * After the call, the structure pointed to by \c pInfo can
+   * be used to create an image with identical properties.
+   * 
+   * If \c pLayout is not \c nullptr, it will receive the
+   * layout that the image will be in after flushing any
+   * outstanding commands on the device.
+   * \param [out] pHandle The image handle
+   * \param [out] pLayout Image layout
+   * \param [out] pInfo Image properties
+   * \returns \c S_OK on success, or \c E_INVALIDARG
+   */
+  virtual HRESULT STDMETHODCALLTYPE GetVulkanImageInfo(
+          VkImage*              pHandle,
+          VkImageLayout*        pLayout,
+          VkImageCreateInfo*    pInfo) = 0;
+};
+
+
+/**
  * \brief DXGI device interface for Vulkan interop
  * 
  * Provides access to the device and instance handles
@@ -167,6 +210,24 @@ IDXGIVkInteropDevice : public IUnknown {
           uint32_t*             pQueueFamilyIndex) = 0;
   
   /**
+   * \brief Transitions a surface to a given layout
+   * 
+   * Executes an explicit image layout transition on the
+   * D3D device. Note that the image subresources \e must
+   * be transitioned back to its original layout before
+   * using it again from D3D11.
+   * \param [in] pSurface The image to transform
+   * \param [in] pSubresources Subresources to transform
+   * \param [in] OldLayout Current image layout
+   * \param [in] NewLayout Desired image layout
+   */
+  virtual HRESULT STDMETHODCALLTYPE TransitionSurfaceLayout(
+          IDXGIVkInteropSurface*    pSurface,
+    const VkImageSubresourceRange*  pSubresources,
+          VkImageLayout             OldLayout,
+          VkImageLayout             NewLayout) = 0;
+  
+  /**
    * \brief Flushes outstanding D3D rendering commands
    * 
    * Must be called before submitting Vulkan commands
@@ -181,6 +242,10 @@ IDXGIVkInteropDevice : public IUnknown {
    * Should be called immediately before submitting
    * Vulkan commands to the rendering queue in order
    * to prevent DXVK from using the queue.
+   * 
+   * While the submission queue is locked, no D3D11
+   * methods must be called from the locking thread,
+   * or otherwise a deadlock might occur.
    */
   virtual void STDMETHODCALLTYPE LockSubmissionQueue() = 0;
   
@@ -201,10 +266,12 @@ struct __declspec(uuid("7a622cf6-627a-46b2-b52f-360ef3da831c")) IDXGIVkDevice;
 struct __declspec(uuid("5679becd-8547-4d93-96a1-e61a1ce7ef37")) IDXGIVkBackBuffer;
 struct __declspec(uuid("79352328-16f2-4f81-9746-9c2e2ccd43cf")) IDXGIVkPresenter;
 struct __declspec(uuid("e2ef5fa5-dc21-4af7-90c4-f67ef6a09323")) IDXGIVkInteropDevice;
+struct __declspec(uuid("5546cf8c-77e7-4341-b05d-8d4d5000e77d")) IDXGIVkInteropSurface;
 #else
 DXVK_DEFINE_GUID(IDXGIVkAdapter);
 DXVK_DEFINE_GUID(IDXGIVkDevice);
 DXVK_DEFINE_GUID(IDXGIVkBackBuffer);
 DXVK_DEFINE_GUID(IDXGIVkPresenter);
 DXVK_DEFINE_GUID(IDXGIVkInteropDevice);
+DXVK_DEFINE_GUID(IDXGIVkInteropSurface);
 #endif
