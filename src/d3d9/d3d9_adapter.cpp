f851ff33e642567502181de4aa40f87ce07469c8
Gabriel Majeri <gabriel.majeri6@gmail.com>
2018-08-07 14:03:15 +0300

    Refactor D3D9 adapter code

--- src/d3d9/d3d9_adapter.cpp
+++ src/d3d9/d3d9_adapter.cpp
@@ -3,21 +3,42 @@
 #include <cstring>
 
 namespace dxvk {
-  D3D9Adapter::D3D9Adapter(Com<IDXGIAdapter1>&& adapter)
-    : m_adapter(adapter) {
-    if (FAILED(m_adapter->EnumOutputs(0, &m_output)))
+  // Use the first output of an adapter.
+  static IDXGIOutput* getFirstOutput(IDXGIAdapter* adapter) {
+    IDXGIOutput* output;
+    if (FAILED(adapter->EnumOutputs(0, &output)))
       throw DxvkError("No monitors attached to adapter");
+    return output;
+  }
 
-    // Cache the supported display modes for later.
+  // Retrieve the HMONITOR of an output.
+  static HMONITOR getOutputMonitor(IDXGIOutput* output) {
+    DXGI_OUTPUT_DESC desc{};
+    if (FAILED(output->GetDesc(&desc)))
+      throw DxvkError("Failed to retrieve output HMONITOR");
 
+    return desc.Monitor;
+  }
+
+  // Cache the supported display modes for later.
+  static std::vector<DXGI_MODE_DESC> getOutputModes(IDXGIOutput* output) {
     // Note: we just use a common format, and assume the same modes for other formats.
     UINT count = 0;
-    m_output->GetDisplayModeList(DXGI_FORMAT_B8G8R8A8_UNORM, 0, &count, nullptr);
+    output->GetDisplayModeList(DXGI_FORMAT_B8G8R8A8_UNORM, 0, &count, nullptr);
 
-    m_modes.resize(count);
+    std::vector<DXGI_MODE_DESC> modes(count);
 
-    if (FAILED(m_output->GetDisplayModeList(DXGI_FORMAT_B8G8R8A8_UNORM, 0, &count, m_modes.data())))
+    if (FAILED(output->GetDisplayModeList(DXGI_FORMAT_B8G8R8A8_UNORM, 0, &count, modes.data())))
       throw DxvkError("Failed to get display mode list");
+
+    return modes;
+  }
+
+  D3D9Adapter::D3D9Adapter(Com<IDXGIAdapter1>&& adapter)
+    : m_adapter(adapter),
+      m_output(getFirstOutput(m_adapter.ptr())),
+      m_monitor(getOutputMonitor(m_output.ptr())),
+      m_modes(getOutputModes(m_output.ptr())) {
   }
 
   HRESULT D3D9Adapter::GetIdentifier(D3DADAPTER_IDENTIFIER9& ident) {
--- src/d3d9/d3d9_adapter.h
+++ src/d3d9/d3d9_adapter.h
@@ -3,20 +3,21 @@
 #include "d3d9_include.h"
 
 namespace dxvk {
-    class D3D9Adapter {
-    public:
-        D3D9Adapter(Com<IDXGIAdapter1>&& adapter);
+  class D3D9Adapter {
+  public:
+    D3D9Adapter(Com<IDXGIAdapter1>&& adapter);
 
-        HRESULT GetIdentifier(D3DADAPTER_IDENTIFIER9& ident);
+    HRESULT GetIdentifier(D3DADAPTER_IDENTIFIER9& ident);
 
-        UINT GetModeCount() const;
-        void GetMode(UINT index, D3DDISPLAYMODE& mode) const;
+    UINT GetModeCount() const;
+    void GetMode(UINT index, D3DDISPLAYMODE& mode) const;
 
-    private:
-        Com<IDXGIAdapter1> m_adapter;
-        // D3D9 does not have the concept of multiple monitors per GPU,
-        // therefore we only use the first one.
-        Com<IDXGIOutput> m_output;
-        std::vector<DXGI_MODE_DESC> m_modes;
-    };
+  private:
+    Com<IDXGIAdapter1> m_adapter;
+    // D3D9 does not have the concept of multiple monitors per GPU,
+    // therefore we only use the first one.
+    Com<IDXGIOutput> m_output;
+    HMONITOR m_monitor;
+    std::vector<DXGI_MODE_DESC> m_modes;
+  };
 }
