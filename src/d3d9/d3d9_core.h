34708edb51d8f3f6ca96c2e1be7b440b12c71aa1
Gabriel Majeri <gabriel.majeri6@gmail.com>
2018-08-07 19:50:27 +0300

    Add final modifiers to core functions

--- src/d3d9/d3d9_core.h
+++ src/d3d9/d3d9_core.h
@@ -20,54 +20,54 @@ namespace dxvk {
     // Utility function, returns a given graphics adapter.
     D3D9Adapter& GetAdapter(UINT adapter);
 
-    HRESULT STDMETHODCALLTYPE RegisterSoftwareDevice(
-      void *pInitializeFunction) override;
-
     HRESULT STDMETHODCALLTYPE QueryInterface(
-      REFIID riid, void** ppvObject) override;
+      REFIID riid, void** ppvObject) final override;
 
-    UINT STDMETHODCALLTYPE GetAdapterCount() override;
+    HRESULT STDMETHODCALLTYPE RegisterSoftwareDevice(
+      void *pInitializeFunction) final override;
+
+    UINT STDMETHODCALLTYPE GetAdapterCount() final override;
 
     HRESULT STDMETHODCALLTYPE GetAdapterIdentifier(UINT Adapter, DWORD Flags,
-      D3DADAPTER_IDENTIFIER9* pIdentifier) override;
+      D3DADAPTER_IDENTIFIER9* pIdentifier) final override;
 
     UINT STDMETHODCALLTYPE GetAdapterModeCount(UINT Adapter,
-      D3DFORMAT Format) override;
+      D3DFORMAT Format) final override;
 
     HRESULT STDMETHODCALLTYPE EnumAdapterModes(UINT Adapter, D3DFORMAT Format,
-      UINT Mode, D3DDISPLAYMODE* pMode) override;
+      UINT Mode, D3DDISPLAYMODE* pMode) final override;
 
     HRESULT STDMETHODCALLTYPE GetAdapterDisplayMode(UINT Adapter,
-      D3DDISPLAYMODE* pMode) override;
+      D3DDISPLAYMODE* pMode) final override;
 
     HRESULT STDMETHODCALLTYPE CheckDeviceType(UINT Adapter, D3DDEVTYPE DevType,
       D3DFORMAT AdapterFormat, D3DFORMAT BackBufferFormat,
-      BOOL bWindowed) override;
+      BOOL bWindowed) final override;
 
     HRESULT STDMETHODCALLTYPE CheckDeviceFormat(UINT Adapter, D3DDEVTYPE DevType,
       D3DFORMAT AdapterFormat, DWORD Usage,
-      D3DRESOURCETYPE RType, D3DFORMAT CheckFormat) override;
+      D3DRESOURCETYPE RType, D3DFORMAT CheckFormat) final override;
 
     HRESULT STDMETHODCALLTYPE CheckDeviceMultiSampleType(UINT Adapter, D3DDEVTYPE DevType,
       D3DFORMAT SurfaceFormat, BOOL Windowed,
-      D3DMULTISAMPLE_TYPE MultiSampleType, DWORD* pQualityLevels) override;
+      D3DMULTISAMPLE_TYPE MultiSampleType, DWORD* pQualityLevels) final override;
 
     HRESULT STDMETHODCALLTYPE CheckDepthStencilMatch(UINT Adapter, D3DDEVTYPE DevType,
       D3DFORMAT AdapterFormat, D3DFORMAT RenderTargetFormat,
-      D3DFORMAT DepthStencilFormat) override;
+      D3DFORMAT DepthStencilFormat) final override;
 
     HRESULT STDMETHODCALLTYPE CheckDeviceFormatConversion(UINT Adapter, D3DDEVTYPE DevType,
-      D3DFORMAT SourceFormat, D3DFORMAT TargetFormat) override;
+      D3DFORMAT SourceFormat, D3DFORMAT TargetFormat) final override;
 
     HRESULT STDMETHODCALLTYPE GetDeviceCaps(UINT Adapter, D3DDEVTYPE DevType,
-      D3DCAPS9* pCaps) override;
+      D3DCAPS9* pCaps) final override;
 
-    HMONITOR STDMETHODCALLTYPE GetAdapterMonitor(UINT Adapter) override;
+    HMONITOR STDMETHODCALLTYPE GetAdapterMonitor(UINT Adapter) final override;
 
     HRESULT STDMETHODCALLTYPE CreateDevice(UINT Adapter, D3DDEVTYPE DevType,
       HWND hFocusWindow, DWORD BehaviorFlags,
       D3DPRESENT_PARAMETERS* pPresentationParameters,
-      IDirect3DDevice9** ppReturnedDeviceInterface) override;
+      IDirect3DDevice9** ppReturnedDeviceInterface) final override;
 
   private:
     Com<IDXGIFactory1> m_factory;
