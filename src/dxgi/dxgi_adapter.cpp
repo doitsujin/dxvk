9ad8dd600ef65f1ebc9f3a0e9d27842d1736e168
Andrew Eikum <aeikum@codeweavers.com>
2018-11-05 10:31:35 -0600

    [dxgi] MultiByteToWideChar counts length in characters

--- src/dxgi/dxgi_adapter.cpp
+++ src/dxgi/dxgi_adapter.cpp
@@ -161,7 +161,7 @@ namespace dxvk {
     // Convert device name
     std::memset(pDesc->Description, 0, sizeof(pDesc->Description));
     ::MultiByteToWideChar(CP_UTF8, 0, deviceProp.deviceName, -1,
-        pDesc->Description, sizeof(pDesc->Description));
+        pDesc->Description, sizeof(pDesc->Description) / sizeof(*pDesc->Description));
     
     // Get amount of video memory
     // based on the Vulkan heaps
