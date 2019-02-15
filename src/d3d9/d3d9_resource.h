a5a3cd92793c1302854269b0e73d9494a5d83138
Joshua Ashton <joshua@froggi.es>
2019-02-15 11:31:21 +0000

    [d3d9] Add and implement Direct3DResource9

---
+++ src/d3d9/d3d9_resource.h
@@ -0,0 +1,84 @@
+#pragma once
+
+#include "d3d9_device_child.h"
+
+#include "../util/com/com_private_data.h"
+
+namespace dxvk {
+
+  template <typename... Type>
+  class Direct3DResource9 : public Direct3DDeviceChild9<Type...> {
+
+  public:
+
+    Direct3DResource9(Direct3DDevice9Ex* device)
+      : Direct3DDeviceChild9<Type...>{ device }
+      , m_priority{ 0 } {}
+
+    HRESULT STDMETHODCALLTYPE SetPrivateData(
+      REFGUID     refguid,
+      const void* pData,
+      DWORD       SizeOfData,
+      DWORD       Flags) final {
+      HRESULT hr;
+      if (Flags & D3DSPD_IUNKNOWN)
+        hr = m_privateData.setInterface(
+          refguid, pData);
+      else
+        hr = m_privateData.setData(
+          refguid, SizeOfData, pData);
+
+      if (FAILED(hr))
+        return D3DERR_INVALIDCALL;
+
+      return D3D_OK;
+    }
+
+    HRESULT STDMETHODCALLTYPE GetPrivateData(
+      REFGUID     refguid,
+      void*       pData,
+      DWORD*      pSizeOfData) final {
+      HRESULT hr = m_privateData.getData(
+        guid, pDataSize, pData);
+
+      if (FAILED(hr))
+        return D3DERR_INVALIDCALL;
+
+      return D3D_OK;
+    }
+
+    HRESULT STDMETHODCALLTYPE FreePrivateData(REFGUID refguid) final {
+      HRESULT hr = m_privateData.FreePrivateData(refguid);
+
+      if (FAILED(hr))
+        return D3DERR_INVALIDCALL;
+
+      return D3D_OK;
+    }
+
+    DWORD STDMETHODCALLTYPE SetPriority(DWORD PriorityNew) final {
+      DWORD oldPriority = m_priority;
+      m_priority = PriorityNew;
+      return oldPriority;
+    }
+
+    DWORD STDMETHODCALLTYPE GetPriority() final {
+      return m_priority;
+    }
+
+    void STDMETHODCALLTYPE PreLoad() final {
+      Logger::warn("Direct3DResource9::PreLoad: Stub");
+    }
+
+
+  protected:
+
+    DWORD m_priority;
+
+  private:
+
+    ComPrivateData m_privateData;
+
+  };
+
+}
