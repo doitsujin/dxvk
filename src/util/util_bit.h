a35b3d905f1e6fa74981d9b43552a6e322b52e42
Joshua Ashton <joshua@froggi.es>
2019-02-15 12:32:02 +0000

    [util] Implement bit cast.

--- src/util/util_bit.h
+++ src/util/util_bit.h
@@ -15,7 +15,20 @@
 
 #include "util_likely.h"
 
+#include <cstring>
+#include <type_traits>
+
 namespace dxvk::bit {
+
+  template<typename T, typename J>
+  T cast(const J& src) {
+    static_assert(sizeof(T) == sizeof(J));
+    static_assert(std::is_trivially_copyable<J>::value && std::is_trivial<T>::value);
+
+    T dst;
+    std::memcpy(&dst, &src, sizeof(T));
+    return dst;
+  }
   
   template<typename T>
   T extract(T value, uint32_t fst, uint32_t lst) {
