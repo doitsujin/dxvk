c8c5291890d38cdcb12afa973cff4d66478f5101
Clément Guérin <libcg@protonmail.com>
2019-03-10 18:51:53 -0700

    Revert "[dxbc] Use SDiv instead of ShiftRightLogical for index calc on Nvidia"
    This reverts commit 032625882990ae843cbdcd526f3c72087ab31888.

--- src/dxbc/dxbc_compiler.cpp
+++ src/dxbc/dxbc_compiler.cpp
@@ -5122,13 +5122,9 @@ namespace dxvk {
     
     const uint32_t typeId = getVectorTypeId(result.type);
     
-    uint32_t offset = m_moduleInfo.options.useSdivForBufferIndex
-      ? m_module.opSDiv             (typeId, structOffset.id, m_module.consti32(4))
-      : m_module.opShiftRightLogical(typeId, structOffset.id, m_module.consti32(2));
-    
     result.id = m_module.opIAdd(typeId,
       m_module.opIMul(typeId, structId.id, m_module.consti32(structStride / 4)),
-      offset);
+      m_module.opShiftRightLogical(typeId, structOffset.id, m_module.consti32(2)));
     return result;
   }
   
@@ -5138,12 +5134,10 @@ namespace dxvk {
     DxbcRegisterValue result;
     result.type.ctype  = DxbcScalarType::Sint32;
     result.type.ccount = 1;
-    
-    uint32_t typeId = getVectorTypeId(result.type);
-    
-    result.id = m_moduleInfo.options.useSdivForBufferIndex
-      ? m_module.opSDiv             (typeId, byteOffset.id, m_module.consti32(4))
-      : m_module.opShiftRightLogical(typeId, byteOffset.id, m_module.consti32(2));
+    result.id = m_module.opShiftRightLogical(
+      getVectorTypeId(result.type),
+      byteOffset.id,
+      m_module.consti32(2));
     return result;
   }
   
