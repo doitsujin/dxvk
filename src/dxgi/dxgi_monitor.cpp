165686048635689338ba9dbaba043c1c1efad44c
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2019-03-14 15:50:35 +0100

    [dxgi] Remove obsolete global monitor helper functions

--- src/dxgi/dxgi_monitor.cpp
+++ src/dxgi/dxgi_monitor.cpp
@@ -2,10 +2,6 @@
 
 namespace dxvk {
 
-  std::mutex                                         g_monitorMutex;
-  std::unordered_map<HMONITOR, DXGI_VK_MONITOR_DATA> g_monitorData;
-
-
   DxgiMonitorInfo::DxgiMonitorInfo(IUnknown* pParent)
   : m_parent(pParent) {
 
@@ -93,45 +89,6 @@ namespace dxvk {
     }
   }
 
-
-  HRESULT InitMonitorData(
-          HMONITOR                hMonitor,
-    const DXGI_VK_MONITOR_DATA*   pData) {
-    if (!hMonitor || !pData)
-      return E_INVALIDARG;
-    
-    std::lock_guard<std::mutex> lock(g_monitorMutex);
-    auto result = g_monitorData.insert({ hMonitor, *pData });
-
-    return result.second ? S_OK : E_INVALIDARG;
-  }
-
-
-  HRESULT AcquireMonitorData(
-          HMONITOR                hMonitor,
-          DXGI_VK_MONITOR_DATA**  ppData) {
-    InitReturnPtr(ppData);
-
-    if (!hMonitor || !ppData)
-      return E_INVALIDARG;
-    
-    g_monitorMutex.lock();
-
-    auto entry = g_monitorData.find(hMonitor);
-    if (entry == g_monitorData.end()) {
-      g_monitorMutex.unlock();
-      return DXGI_ERROR_NOT_FOUND;
-    }
-
-    *ppData = &entry->second;
-    return S_OK;
-  }
-
-  
-  void ReleaseMonitorData() {
-    g_monitorMutex.unlock();
-  }
-  
 
   HRESULT GetMonitorDisplayMode(
           HMONITOR                hMonitor,
--- src/dxgi/dxgi_monitor.h
+++ src/dxgi/dxgi_monitor.h
@@ -56,39 +56,6 @@ namespace dxvk {
           DXGI_FORMAT             Format);
   
   /**
-   * \brief Initializes monitor data
-   * 
-   * Fails if data for the given monitor already exists.
-   * \param [in] hMonitor The monitor handle
-   * \param [in] pData Initial data
-   */
-  HRESULT InitMonitorData(
-          HMONITOR                hMonitor,
-    const DXGI_VK_MONITOR_DATA*   pData);
-
-  /**
-   * \brief Retrieves and locks monitor data
-   * 
-   * Fails if no data for the given monitor exists.
-   * \param [in] hMonitor The monitor handle
-   * \param [out] Pointer to monitor data
-   * \returns S_OK on success
-   */
-  HRESULT AcquireMonitorData(
-          HMONITOR                hMonitor,
-          DXGI_VK_MONITOR_DATA**  ppData);
-  
-  /**
-   * \brief Unlocks monitor data
-   * 
-   * Must be called after each successful
-   * call to \ref AcquireMonitorData.
-   * \param [in] hMonitor The monitor handle
-   */
-  void ReleaseMonitorData();
-
-
-  /**
    * \brief Retrieves monitor display mode
    *
    * \param [in] hMonitor Monitor handle
