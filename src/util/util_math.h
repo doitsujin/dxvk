d44707e349e06cdceeb7b4a281bf2a67b8ca1b50
Joshua Ashton <joshua@froggi.es>
2019-03-24 20:38:39 +0000

    [util] Mark clamp & align as constexpr

--- src/util/util_math.h
+++ src/util/util_math.h
@@ -5,14 +5,14 @@ namespace dxvk {
   constexpr size_t CACHE_LINE_SIZE = 64;
   
   template<typename T>
-  T clamp(T n, T lo, T hi) {
+  constexpr T clamp(T n, T lo, T hi) {
     if (n < lo) return lo;
     if (n > hi) return hi;
     return n;
   }
   
   template<typename T, typename U = T>
-  T align(T what, U to) {
+  constexpr T align(T what, U to) {
     return (what + to - 1) & ~(to - 1);
   }
   
