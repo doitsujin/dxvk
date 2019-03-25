676b4880ae1837d0ff608a9e3bd62657e270f670
Joshua Ashton <joshua@froggi.es>
2019-03-25 22:14:20 +0000

    [d3d9] Correct constant set size

--- src/d3d9/d3d9_constant_set.h
+++ src/d3d9/d3d9_constant_set.h
@@ -8,7 +8,7 @@ namespace dxvk {
   class Direct3DCommonBuffer9;
 
   struct D3D9ConstantSets {
-    constexpr static uint32_t SetSize    = (256 + 16 + 16) * sizeof(uint32_t);
+    constexpr static uint32_t SetSize    = (4 * (256 + 16) + 16) * sizeof(uint32_t);
     constexpr static uint32_t SetAligned = align(SetSize, 256);
     constexpr static uint32_t SetCount   = 8; // Magic number for now. May change later
     constexpr static uint32_t BufferSize = SetAligned * SetCount;
