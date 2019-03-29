b2daa78a10bbbee23f2978bfaf94a70a7eff986b
Joshua Ashton <joshua@froggi.es>
2019-03-29 02:14:54 +0000

    [d3d9] Fix vertex sampler slot indices

--- src/d3d9/d3d9_util.h
+++ src/d3d9/d3d9_util.h
@@ -27,7 +27,7 @@ namespace dxvk {
   }
 
   inline std::pair<DxsoProgramType, DWORD> RemapStateSamplerShader(DWORD Sampler) {
-    if (Sampler > 16)
+    if (Sampler >= 16)
       return std::make_pair(DxsoProgramType::VertexShader, Sampler - 16);
 
     return std::make_pair(DxsoProgramType::PixelShader, Sampler);
