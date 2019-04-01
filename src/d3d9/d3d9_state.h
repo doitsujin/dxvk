b76c44a32fa22e38a1cd34fdd85bb4c6a505f4bb
Joshua Ashton <joshua@froggi.es>
2019-04-01 23:51:18 +0100

    [d3d9] Segregate capturable state and define capture tracking struct

--- src/d3d9/d3d9_state.h
+++ src/d3d9/d3d9_state.h
@@ -4,8 +4,13 @@
 #include "d3d9_constant_set.h"
 
 #include <array>
+#include <bitset>
 
 namespace dxvk {
+
+  static constexpr uint32_t RenderStateCount  = D3DRS_BLENDOPALPHA + 1;
+  static constexpr uint32_t SamplerStateCount = D3DSAMP_DMAPOFFSET + 1;
+  static constexpr uint32_t SamplerCount      = 21;
 
   class Direct3DSurface9;
   class D3D9VertexShader;
@@ -19,57 +24,94 @@ namespace dxvk {
   };
 
   struct D3D9RenderStateInfo {
-    float alphaRef;
+    float alphaRef = 0.0f;
   };
   
   struct D3D9VBO {
-    D3D9VBO() {
-      vertexBuffer = nullptr;
-    }
-
-    Direct3DVertexBuffer9* vertexBuffer;
-    UINT offset;
-    UINT stride;
+    Direct3DVertexBuffer9* vertexBuffer = nullptr;
+    UINT                   offset = 0;
+    UINT                   stride = 0;
   };
 
-  struct Direct3DState9 {
-    Direct3DState9() {
-      for (uint32_t i = 0; i < renderTargets.size(); i++)
-        renderTargets[i] = nullptr;
+  struct D3D9CapturableState {
+    D3D9CapturableState() {
+      for (uint32_t i = 0; i < textures.size(); i++)
+        textures[i] = nullptr;
 
-      depthStencil = nullptr;
-      vertexShader = nullptr;
-      pixelShader = nullptr;
-      vertexDecl = nullptr;
-      indices = nullptr;
-      
       for (uint32_t i = 0; i < clipPlanes.size(); i++)
         clipPlanes[i] = D3D9ClipPlane();
     }
 
+    Direct3DVertexDeclaration9*                      vertexDecl = nullptr;
+    Direct3DIndexBuffer9*                            indices    = nullptr;
+
+    std::array<DWORD, RenderStateCount>              renderStates;
+
+    std::array<
+      std::array<DWORD, SamplerStateCount>,
+      SamplerCount>                                  samplerStates;
+
+    std::array<D3D9VBO, caps::MaxStreams>            vertexBuffers;
+
+    std::array<
+      IDirect3DBaseTexture9*,
+      SamplerCount>                                  textures;
+
+    D3D9VertexShader*                                vertexShader = nullptr;
+    D3D9PixelShader*                                 pixelShader  = nullptr;
+
+    D3DVIEWPORT9                                     viewport;
+    RECT                                             scissorRect;
+
+    std::array<
+      D3D9ClipPlane,
+      caps::MaxClipPlanes>                           clipPlanes;
+
+    D3D9ShaderConstants                              vsConsts;
+    D3D9ShaderConstants                              psConsts;
+  };
+
+  enum class CapturedStateFlag : uint32_t {
+    VertexDecl,
+    Indices,
+    RenderStates,
+    SamplerStates,
+    VertexBuffers,
+    Textures,
+    VertexShader,
+    PixelShader,
+    Viewport,
+    ScissorRect,
+    ClipPlanes,
+    VsConstants,
+    PsConstants
+  };
+
+  using CapturedStateFlags = Flags<CapturedStateFlag>;
+
+  struct D3D9StateCaptures {
+    CapturedStateFlags flags;
+
+    std::bitset<RenderStateCount>                       renderStates;
+
+    std::array<
+      std::bitset<SamplerStateCount>,
+      SamplerCount>                                     samplerStates;
+
+    std::bitset<caps::MaxStreams>                       vertexBuffers;
+    std::bitset<SamplerCount>                           textures;
+    std::bitset<caps::MaxClipPlanes>                    clipPlanes;
+  };
+
+  struct Direct3DState9 : public D3D9CapturableState {
+    Direct3DState9() {
+      for (uint32_t i = 0; i < renderTargets.size(); i++)
+        renderTargets[i] = nullptr;
+    }
+
     std::array<Direct3DSurface9*, caps::MaxSimultaneousRenderTargets> renderTargets;
-    std::array<D3D9VBO, caps::MaxStreams> vertexBuffers;
 
-    Direct3DIndexBuffer9* indices;
-
-    Direct3DSurface9* depthStencil;
-
-    D3D9VertexShader* vertexShader;
-    D3D9PixelShader* pixelShader;
-
-    Direct3DVertexDeclaration9* vertexDecl;
-
-    D3DVIEWPORT9 viewport;
-    RECT scissorRect;
-
-    std::array<DWORD, D3DRS_BLENDOPALPHA + 1> renderStates;
-    std::array<std::array<DWORD, D3DSAMP_DMAPOFFSET + 1>, 21> samplerStates;
-    std::array<IDirect3DBaseTexture9*, 21> textures;
-
-    D3D9ShaderConstants vsConsts;
-    D3D9ShaderConstants psConsts;
-    
-    std::array<D3D9ClipPlane, caps::MaxClipPlanes> clipPlanes;
+    Direct3DSurface9* depthStencil = nullptr;
     
   };
 
