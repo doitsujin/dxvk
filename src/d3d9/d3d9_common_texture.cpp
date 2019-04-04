074efe52b0799e74bd76528fd32354754dcaf1ad
Joshua Ashton <joshua@froggi.es>
2019-04-05 00:20:00 +0100

    [d3d9] Use a buffer if we have more than 1 mip, even if we are offscreen

--- src/d3d9/d3d9_common_texture.cpp
+++ src/d3d9/d3d9_common_texture.cpp
@@ -370,7 +370,7 @@ namespace dxvk {
     if (GetPackedDepthStencilFormat(m_desc.Format))
       return D3D9_COMMON_TEXTURE_MAP_MODE_BUFFER;
 
-    if (!m_desc.Offscreen || m_desc.Pool != D3DPOOL_SYSTEMMEM || m_desc.Pool != D3DPOOL_SCRATCH)
+    if (!m_desc.Offscreen || m_desc.Pool != D3DPOOL_SYSTEMMEM || m_desc.Pool != D3DPOOL_SCRATCH || m_desc.MipLevels > 1)
       return D3D9_COMMON_TEXTURE_MAP_MODE_BUFFER;
 
     // Images that can be read by the host should be mapped directly in
