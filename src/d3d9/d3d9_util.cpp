b4c65a7c9b627140917fad135e00e09280e83d9d
Joshua Ashton <joshua@froggi.es>
2019-04-15 19:16:32 +0100

    [d3d9] Cleanup DecodeMultiSampleType

--- src/d3d9/d3d9_util.cpp
+++ src/d3d9/d3d9_util.cpp
@@ -43,11 +43,11 @@ namespace dxvk {
     switch (MultiSample) {
     case D3DMULTISAMPLE_NONE:
     case D3DMULTISAMPLE_NONMASKABLE: flag = VK_SAMPLE_COUNT_1_BIT;  break;
-    case D3DMULTISAMPLE_2_SAMPLES: flag = VK_SAMPLE_COUNT_2_BIT;  break;
-    case D3DMULTISAMPLE_4_SAMPLES: flag = VK_SAMPLE_COUNT_4_BIT;  break;
-    case D3DMULTISAMPLE_8_SAMPLES: flag = VK_SAMPLE_COUNT_8_BIT;  break;
-    case D3DMULTISAMPLE_16_SAMPLES: flag = VK_SAMPLE_COUNT_16_BIT; break;
-    default: return D3DERR_INVALIDCALL;
+    case D3DMULTISAMPLE_2_SAMPLES:   flag = VK_SAMPLE_COUNT_2_BIT;  break;
+    case D3DMULTISAMPLE_4_SAMPLES:   flag = VK_SAMPLE_COUNT_4_BIT;  break;
+    case D3DMULTISAMPLE_8_SAMPLES:   flag = VK_SAMPLE_COUNT_8_BIT;  break;
+    case D3DMULTISAMPLE_16_SAMPLES:  flag = VK_SAMPLE_COUNT_16_BIT; break;
+    default:                         return D3DERR_INVALIDCALL;
     }
 
     if (pCount != nullptr)
