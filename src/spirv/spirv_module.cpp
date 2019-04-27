e144c17363ded7251c4f5a547c8c2b0ce1373a75
Joshua Ashton <joshua@froggi.es>
2019-04-27 15:26:37 +0100

    [spirv] Implement constfReplicant helper

--- src/spirv/spirv_module.cpp
+++ src/spirv/spirv_module.cpp
@@ -342,6 +342,24 @@ namespace dxvk {
     
     return this->constComposite(vectorTypeId, args.size(), args.data());
   }
+
+
+  uint32_t SpirvModule::constfReplicant(
+          float                   replicant,
+          uint32_t                count) {
+    uint32_t value = this->constf32(replicant);
+
+    std::array<uint32_t, 4> args = { value, value, value, value };
+
+    // Can't make a scalar composite.
+    if (count == 1)
+      return args[0];
+    
+    uint32_t scalarTypeId = this->defFloatType(32);
+    uint32_t vectorTypeId = this->defVectorType(scalarTypeId, count);
+    
+    return this->constComposite(vectorTypeId, count, args.data());
+  }
   
   
   uint32_t SpirvModule::constComposite(
--- src/spirv/spirv_module.h
+++ src/spirv/spirv_module.h
@@ -160,6 +160,10 @@ namespace dxvk {
             float                   y,
             float                   z,
             float                   w);
+
+    uint32_t constfReplicant(
+            float                   replicant,
+            uint32_t                count);
     
     uint32_t constComposite(
             uint32_t                typeId,
