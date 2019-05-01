95ac4d2af4e6ad8aa44683c22f8fb69848ad8e38
Joshua Ashton <joshua@froggi.es>
2019-05-01 02:24:38 +0100

    [d3d9] Add extra formats to GetMonitorFormatBpp

--- src/d3d9/d3d9_monitor.cpp
+++ src/d3d9/d3d9_monitor.cpp
@@ -6,10 +6,12 @@ namespace dxvk {
 
   uint32_t GetMonitorFormatBpp(D3D9Format Format) {
     switch (Format) {
+    case D3D9Format::A8R8G8B8:
     case D3D9Format::X8R8G8B8: // This is still 32 bit even though the alpha is unspecified.
     case D3D9Format::A2R10G10B10:
       return 32;
 
+    case D3D9Format::A1R5G5B5:
     case D3D9Format::X1R5G5B5:
     case D3D9Format::R5G6B5:
       return 16;
