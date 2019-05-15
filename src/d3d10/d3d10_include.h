3e28b571457d573221b995390fc7e4e14c3aab8f
Edmondo Tommasina <edmondo.tommasina@gmail.com>
2019-05-15 21:10:24 +0200

    [d3d10] define IID_PPV_ARGS macro for winelib build

--- src/d3d10/d3d10_include.h
+++ src/d3d10/d3d10_include.h
@@ -5,3 +5,14 @@
 
 #include <d3d10_1.h>
 #include <d3d11_1.h>
+
+#ifdef __WINE__
+extern "C++" {
+  template<typename T> void **IID_PPV_ARGS_Helper (T **pp) {
+    static_cast<IUnknown *> (*pp);
+    return reinterpret_cast<void **> (pp);
+  }
+}
+
+#define IID_PPV_ARGS(ppType) __uuidof (**(ppType)), IID_PPV_ARGS_Helper (ppType)
+#endif // __WINE__
