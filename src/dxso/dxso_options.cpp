b838a16246f1d64c298cda9d119f89343326be97
Joshua Ashton <joshua@froggi.es>
2019-07-08 17:26:41 +0100

    [dxso] Enable early discard on RADV/ACO
    Still can't enable on LLVM due to GPU hangs. The way we detect it
    may have to change in the future, but for now, this should do.
    
    Mirrors DXBC commit

--- src/dxso/dxso_options.cpp
+++ src/dxso/dxso_options.cpp
@@ -13,10 +13,12 @@ namespace dxvk {
       && (devInfo.coreSubgroup.supportedStages & VK_SHADER_STAGE_FRAGMENT_BIT)
       && (devInfo.coreSubgroup.supportedOperations & VK_SUBGROUP_FEATURE_BALLOT_BIT);
 
-    // Disable early discard on RADV due to GPU hangs
+    // Disable early discard on RADV (with LLVM) due to GPU hangs
     // Disable early discard on Nvidia because it may hurt performance
-    if (adapter->matchesDriver(DxvkGpuVendor::Amd,    VK_DRIVER_ID_MESA_RADV_KHR,          0, 0)
-     || adapter->matchesDriver(DxvkGpuVendor::Nvidia, VK_DRIVER_ID_NVIDIA_PROPRIETARY_KHR, 0, 0))
+    bool isRadvAco = std::string(devInfo.core.properties.deviceName).find("RADV/ACO") != std::string::npos;
+
+    if ((adapter->matchesDriver(DxvkGpuVendor::Amd,    VK_DRIVER_ID_MESA_RADV_KHR,          0, 0) && !isRadvAco)
+     || (adapter->matchesDriver(DxvkGpuVendor::Nvidia, VK_DRIVER_ID_NVIDIA_PROPRIETARY_KHR, 0, 0)))
       useSubgroupOpsForEarlyDiscard = false;
     
     // Apply shader-related options
