9a2da555c0a6306ddfe470b793849e867350c95a
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2019-07-30 19:29:02 +0200

    [util] Add helper for lazy initialization
    a

---
+++ src/util/util_lazy.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <mutex>
+
+namespace dxvk {
+
+  /**
+   * \brief Lazy-initialized object
+   * 
+   * Creates an object on demand with
+   * the given constructor arguments.
+   */
+  template<typename T>
+  class Lazy {
+
+  public:
+
+    template<typename... Args>
+    T& get(Args... args) {
+      if (m_object)
+        return *m_object;
+
+      std::lock_guard lock(m_mutex);
+
+      if (!m_object) {
+        m_object = std::make_unique<T>(
+          std::forward<Args>(args)...);
+      }
+
+      return *m_object;
+    }
+
+  private:
+
+    std::mutex         m_mutex;
+    std::unique_ptr<T> m_object;
+
+  };
+
+}
