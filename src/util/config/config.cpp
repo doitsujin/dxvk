f5dc9404a2d14c9b05d586d913ee508171dd5f0f
Robin Kertels <robin.kertels@gmail.com>
2019-09-16 01:38:23 +0200

    [util] Disable read only locking for Burnout Paradise
    Fixes the splash screen and the background on
    a couple of menu screens

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -231,6 +231,10 @@ namespace dxvk {
     { R"(\\Dead Space\.exe$)", {{
       { "d3d9.supportDFFormats",                 "False" },
     }} },
+    /* Burnout Paradise                           */
+    { R"(\\BurnoutParadise\.exe$)", {{
+      { "d3d9.allowLockFlagReadonly",       "False" },
+    }} },
   }};
 
 
