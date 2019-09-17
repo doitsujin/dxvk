b8a0593c708aa321e0353ee045a678f1b25950d1
Joshua Ashton <joshua@froggi.es>
2019-09-17 03:43:43 +0100

    [d3d9] Prefer OPAQUE_WHITE border color over TRANSPARENT_BLACK for (0, 0, 0, 1)
    Fixes black textures in #317

--- src/d3d9/d3d9_device.cpp
+++ src/d3d9/d3d9_device.cpp
@@ -5130,6 +5130,15 @@ namespace dxvk {
       for (uint32_t i = 0; i < 4; i++)
         colorInfo.borderColor.float32[i] = cKey.BorderColor[i];
 
+      // HACK: Let's get OPAQUE_WHITE border color over
+      // TRANSPARENT_BLACK if the border RGB is white.
+      if (colorInfo.borderColor.float32[0] == 1.0f
+       && colorInfo.borderColor.float32[1] == 1.0f
+       && colorInfo.borderColor.float32[2] == 1.0f) {
+        // Then set the alpha to 1.
+        colorInfo.borderColor.float32[3] = 1.0f;
+      }
+
       DxvkSamplerCreateInfo depthInfo = colorInfo;
       depthInfo.compareToDepth = VK_TRUE;
       depthInfo.compareOp      = VK_COMPARE_OP_LESS_OR_EQUAL;
