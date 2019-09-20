560139431f0829ddddf97e9e3aab597bde6644fe
Joshua Ashton <joshua@froggi.es>
2019-09-20 03:59:26 +0100

    [d3d9] Fix alignment of option definitions

--- src/d3d9/d3d9_options.cpp
+++ src/d3d9/d3d9_options.cpp
@@ -34,24 +34,24 @@ namespace dxvk {
     this->customVendorId        = parsePciId(config.getOption<std::string>("d3d9.customVendorId"));
     this->customDeviceId        = parsePciId(config.getOption<std::string>("d3d9.customDeviceId"));
 
-    this->maxFrameLatency       = config.getOption<int32_t>("d3d9.maxFrameLatency", 0);
-    this->presentInterval       = config.getOption<int32_t>("d3d9.presentInterval", -1);
-    this->shaderModel           = config.getOption<int32_t>("d3d9.shaderModel",     3);
-    this->evictManagedOnUnlock  = config.getOption<bool>   ("d3d9.evictManagedOnUnlock", false);
-    this->dpiAware              = config.getOption<bool>   ("d3d9.dpiAware", true);
-    this->allowLockFlagReadonly = config.getOption<bool>   ("d3d9.allowLockFlagReadonly", true);
-    this->strictConstantCopies  = config.getOption<bool>   ("d3d9.strictConstantCopies", false);
-    this->strictPow             = config.getOption<bool>   ("d3d9.strictPow",            true);
-    this->lenientClear          = config.getOption<bool>   ("d3d9.lenientClear",         false);
-    this->numBackBuffers        = config.getOption<int32_t>("d3d9.numBackBuffers", 0);
-    this->deferSurfaceCreation  = config.getOption<bool>   ("d3d9.deferSurfaceCreation", false);
-    this->hasHazards            = config.getOption<bool>   ("d3d9.hasHazards",           false);
-    this->samplerAnisotropy     = config.getOption<int32_t>("d3d9.samplerAnisotropy", -1);
-    this->maxAvailableMemory    = config.getOption<uint32_t>("d3d9.maxAvailableMemory", UINT32_MAX);
-    this->supportDFFormats      = config.getOption<bool>("d3d9.supportDFFormats", true);
-    this->swvpFloatCount        = config.getOption<uint32_t>("d3d9.swvpFloatCount", caps::MaxFloatConstantsSoftware);
-    this->swvpIntCount          = config.getOption<uint32_t>("d3d9.swvpIntCount",   caps::MaxOtherConstantsSoftware);
-    this->swvpBoolCount         = config.getOption<uint32_t>("d3d9.swvpBoolCount",  caps::MaxOtherConstantsSoftware);
+    this->maxFrameLatency       = config.getOption<int32_t> ("d3d9.maxFrameLatency",       0);
+    this->presentInterval       = config.getOption<int32_t> ("d3d9.presentInterval",       -1);
+    this->shaderModel           = config.getOption<int32_t> ("d3d9.shaderModel",           3);
+    this->evictManagedOnUnlock  = config.getOption<bool>    ("d3d9.evictManagedOnUnlock",  false);
+    this->dpiAware              = config.getOption<bool>    ("d3d9.dpiAware",              true);
+    this->allowLockFlagReadonly = config.getOption<bool>    ("d3d9.allowLockFlagReadonly", true);
+    this->strictConstantCopies  = config.getOption<bool>    ("d3d9.strictConstantCopies",  false);
+    this->strictPow             = config.getOption<bool>    ("d3d9.strictPow",             true);
+    this->lenientClear          = config.getOption<bool>    ("d3d9.lenientClear",          false);
+    this->numBackBuffers        = config.getOption<int32_t> ("d3d9.numBackBuffers",        0);
+    this->deferSurfaceCreation  = config.getOption<bool>    ("d3d9.deferSurfaceCreation",  false);
+    this->hasHazards            = config.getOption<bool>    ("d3d9.hasHazards",            false);
+    this->samplerAnisotropy     = config.getOption<int32_t> ("d3d9.samplerAnisotropy",     -1);
+    this->maxAvailableMemory    = config.getOption<uint32_t>("d3d9.maxAvailableMemory",    UINT32_MAX);
+    this->supportDFFormats      = config.getOption<bool>    ("d3d9.supportDFFormats",      true);
+    this->swvpFloatCount        = config.getOption<uint32_t>("d3d9.swvpFloatCount",        caps::MaxFloatConstantsSoftware);
+    this->swvpIntCount          = config.getOption<uint32_t>("d3d9.swvpIntCount",          caps::MaxOtherConstantsSoftware);
+    this->swvpBoolCount         = config.getOption<uint32_t>("d3d9.swvpBoolCount",         caps::MaxOtherConstantsSoftware);
 
     this->d3d9FloatEmulation    = true; // <-- Future Extension?
 
