f498e742ec6fb2c3face037f6a22a726be257106
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2019-09-23 15:20:53 +0200

    [d3d11] Handle D3D_PRIMITIVE_TOPOLOGY_UNDEFINED correctly
    Luckily, all known cases where games use UNDEFINED topology fail
    validation elsewhere due to missing vertex shaders, but we should
    handle this correctly anyway.

--- src/d3d11/d3d11_context.cpp
+++ src/d3d11/d3d11_context.cpp
@@ -3201,7 +3201,7 @@ namespace dxvk {
 
     if (topology <= D3D_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP_ADJ) {
       static const std::array<DxvkInputAssemblyState, 14> s_iaStates = {{
-        { }, // D3D_PRIMITIVE_TOPOLOGY_UNDEFINED
+        { VK_PRIMITIVE_TOPOLOGY_MAX_ENUM,       VK_FALSE, 0 },
         { VK_PRIMITIVE_TOPOLOGY_POINT_LIST,     VK_FALSE, 0 },
         { VK_PRIMITIVE_TOPOLOGY_LINE_LIST,      VK_FALSE, 0 },
         { VK_PRIMITIVE_TOPOLOGY_LINE_STRIP,     VK_TRUE,  0 },
