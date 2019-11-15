73a5b333754c3fd642608f5ccfcf98b8ff3f6a6b
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2019-11-15 19:48:41 +0100

    [d3d11] Also fix reset counter memory order for deferred context queries

--- src/d3d11/d3d11_query.h
+++ src/d3d11/d3d11_query.h
@@ -55,7 +55,7 @@ namespace dxvk {
 
     void DoDeferredEnd() {
       m_state = D3D11_VK_QUERY_ENDED;
-      m_resetCtr += 1;
+      m_resetCtr.fetch_add(1, std::memory_order_acquire);
     }
 
     bool IsScoped() const {
