edc8f339edf9c7866cc54ce16b2bb1bafd20ff67
Joshua Ashton <joshua@froggi.es>
2019-11-16 23:01:44 +0000

    [util] Use native directory slash for delimiting exe name

--- src/util/util_env.cpp
+++ src/util/util_env.cpp
@@ -2,6 +2,13 @@
 
 namespace dxvk::env {
 
+#ifndef DXVK_NATIVE
+  constexpr char dirSlash = '\\';
+#else
+  constexpr char dirSlash = '/';
+#endif
+
+
   std::string getEnvVar(const char* name) {
     char* result = std::getenv(name);
     return (result)
@@ -12,7 +19,7 @@ namespace dxvk::env {
   
   std::string getExeName() {
     std::string fullPath = getExePath();
-    auto n = fullPath.find_last_of('\\');
+    auto n = fullPath.find_last_of(dirSlash);
     
     return (n != std::string::npos)
       ? fullPath.substr(n + 1)
