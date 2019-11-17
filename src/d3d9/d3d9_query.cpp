93de93a3bdaf93813a92ee5a7c2ddc4b21835423
Joshua Ashton <joshua@froggi.es>
2019-11-17 17:52:22 +0000

    [d3d9] Fix memory order for query reset counter
    Probably doesn't change anything, but let's fix it anyway.

--- src/d3d9/d3d9_query.cpp
+++ src/d3d9/d3d9_query.cpp
@@ -114,7 +114,7 @@ namespace dxvk {
         if (m_state != D3D9_VK_QUERY_BEGUN && QueryBeginnable(m_queryType))
           m_parent->Begin(this);
 
-        m_resetCtr += 1;
+        m_resetCtr.fetch_add(1, std::memory_order_acquire);
 
         m_parent->End(this);
 
@@ -273,7 +273,7 @@ namespace dxvk {
       default: break;
     }
 
-    m_resetCtr -= 1;
+    m_resetCtr.fetch_sub(1, std::memory_order_release);
   }
 
 
