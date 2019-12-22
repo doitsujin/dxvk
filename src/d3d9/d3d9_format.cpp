05de0b20a09983d62bc564b531bc801cbfaef42f
Joshua Ashton <joshua@froggi.es>
2019-12-22 01:43:36 +0000

    [d3d9] Use VK_FORMAT_D24_UNORM_S8_UINT for D3DFMT_D24X8
    May improve performance in some instances

--- src/d3d9/d3d9_format.cpp
+++ src/d3d9/d3d9_format.cpp
@@ -218,7 +218,7 @@ namespace dxvk {
         VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT };
 
       case D3D9Format::D24X8: return {
-        VK_FORMAT_D32_SFLOAT,
+        VK_FORMAT_D24_UNORM_S8_UINT,
         VK_FORMAT_UNDEFINED,
         VK_IMAGE_ASPECT_DEPTH_BIT };
 
