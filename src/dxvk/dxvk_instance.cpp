4308d9b4b3d223885134e224de55c0b0d1f37021
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2020-01-09 20:10:58 +0100

    [meta] Release 1.5.1

--- src/dxvk/dxvk_instance.cpp
+++ src/dxvk/dxvk_instance.cpp
@@ -119,7 +119,7 @@ namespace dxvk {
     appInfo.pApplicationName      = appName.c_str();
     appInfo.applicationVersion    = 0;
     appInfo.pEngineName           = "DXVK";
-    appInfo.engineVersion         = VK_MAKE_VERSION(1, 5, 0);
+    appInfo.engineVersion         = VK_MAKE_VERSION(1, 5, 1);
     appInfo.apiVersion            = VK_MAKE_VERSION(1, 1, 0);
     
     VkInstanceCreateInfo info;
