146cba379bbb5f4a9611067e71e106f0e7ff39d4
Joshua Ashton <joshua@froggi.es>
2020-02-16 04:59:24 +0000

    [tests] Add test for D3D10[Core]RegisterLayers
    Calling this function as a HRESULT type, gives us a E_NOTIMPL value
    99% confident this is at least the correct return type given that.
    No idea what it takes in though.

--- tests/d3d10/test_d3d10_core.cpp
+++ tests/d3d10/test_d3d10_core.cpp
@@ -17,6 +17,11 @@ using namespace dxvk;
 // which matches the values of eax and edx which is consistent to some
 // function returing a UINT64 value across these arches.
 using pfnD3D10CoreGetVersion = UINT64(_stdcall *)();
+
+// Calling this function as a HRESULT type, gives us a E_NOTIMPL value
+// 99% confident this is at least the correct return type given that.
+// No idea what it takes in though.
+using pfnD3D10CoreRegisterLayers = HRESULT(_stdcall*)();
   
 int main(int argc, char** argv) {
   HMODULE hD3D10     = LoadLibraryA("d3d10.dll");
@@ -44,6 +49,25 @@ int main(int argc, char** argv) {
   std::cout << "(d3d10core.dll) D3D10CoreGetVersion: " << std::hex << D3D10CoreGetVersion() << std::endl;
 
   std::cout << "(d3d10_1.dll) D3D10GetVersion: " << std::hex << D3D10GetVersion1() << std::endl;
+
+
+  std::cout << std::endl;
+
+
+  auto* D3D10RegisterLayers = reinterpret_cast<pfnD3D10CoreRegisterLayers>(
+    GetProcAddress(hD3D10, "D3D10RegisterLayers"));
+
+  auto* D3D10CoreRegisterLayers = reinterpret_cast<pfnD3D10CoreRegisterLayers>(
+    GetProcAddress(hD3D10Core, "D3D10CoreRegisterLayers"));
+
+  auto* D3D10RegisterLayers1 = reinterpret_cast<pfnD3D10CoreRegisterLayers>(
+    GetProcAddress(hD3D10_1, "D3D10RegisterLayers"));
+
+  std::cout << "(d3d10.dll) D3D10RegisterLayers: " << std::hex << D3D10RegisterLayers() << std::endl;
+
+  std::cout << "(d3d10core.dll) D3D10CoreRegisterLayers: " << std::hex << D3D10CoreRegisterLayers() << std::endl;
+
+  std::cout << "(d3d10_1.dll) D3D10RegisterLayers: " << std::hex << D3D10RegisterLayers1() << std::endl;
 
   return 0;
 }
