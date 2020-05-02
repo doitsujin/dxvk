7239066c5c3f4e0a9b45233cfb9562a3254bb68f
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2020-05-02 20:20:56 +0200

    [dxvk] Don't stall SDMA command buffer for image initialization

--- src/dxvk/dxvk_context.cpp
+++ src/dxvk/dxvk_context.cpp
@@ -2090,8 +2090,7 @@ namespace dxvk {
     // Discard previous subresource contents
     m_sdmaAcquires.accessImage(image,
       vk::makeSubresourceRange(subresources),
-      VK_IMAGE_LAYOUT_UNDEFINED,
-      VK_PIPELINE_STAGE_TRANSFER_BIT, 0,
+      VK_IMAGE_LAYOUT_UNDEFINED, 0, 0,
       image->pickLayout(VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL),
       VK_PIPELINE_STAGE_TRANSFER_BIT,
       VK_ACCESS_TRANSFER_WRITE_BIT);
