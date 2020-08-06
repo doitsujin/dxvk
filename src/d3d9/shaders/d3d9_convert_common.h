5561a6a1ccb7472c98ed25b84200a610483c5f13
Joshua Ashton <joshua@froggi.es>
2020-08-06 07:19:50 +0100

    [d3d9] Move some YUV helpers to common and cleanup YUY2 shader

--- src/d3d9/shaders/d3d9_convert_common.h
+++ src/d3d9/shaders/d3d9_convert_common.h
@@ -9,5 +9,21 @@ float snormalize(int value, int bits) {
 
   // Min because, -32 and -31 map to -1.0f, and we
   // divide by 31.
-  return max(float(value) / float(range), -1.0f);
+  return max(float(value) / float(range), -1.0);
+}
+
+float unpackUnorm(uint p) {
+  return float(p) / 255.0;
+}
+
+mat3x4 g_yuv_to_rgb = {
+  { 298 / 256,  0,          409 / 256, 0.5 },
+  { 298 / 256, -100 / 256, -208 / 256, 0.5 },
+  { 298 / 256,  516 / 256,  0,         0.5 }
+};
+
+vec4 convertYUV(vec3 yuv) {
+  vec3 value = vec4(yuv, 1 / 255.0) * g_yuv_to_rgb;
+
+  return vec4(clamp(value, 0, 1), 1);
 }
