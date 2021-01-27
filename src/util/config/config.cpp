5f9a6c5f94acd61dd06bdcd43dcceb868ea828a3
dom <dom@dom.vg>
2021-01-27 14:04:29 +0100

    [util] Enable dxgi.customVendorId for Hitman 3

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -117,8 +117,8 @@ namespace dxvk {
     { R"(\\vr\.exe$)", {{
       { "d3d11.dcSingleUseMode",            "False" },
     }} },
-    /* Hitman 2 - requires AGS library            */
-    { R"(\\HITMAN2\.exe$)", {{
+    /* Hitman 2 and 3 - requires AGS library      */
+    { R"(\\HITMAN(2|3)\.exe$)", {{
       { "dxgi.customVendorId",              "10de" },
     }} },
     /* Modern Warfare Remastered                  */
