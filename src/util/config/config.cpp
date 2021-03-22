2ba433bbd80fdb9d5b6cdb7411112a22883a7a1e
Robin Kertels <robin.kertels@gmail.com>
2021-03-22 14:08:13 +0100

    [d3d9+util] Remove option to disable implicit discard

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -395,7 +395,6 @@ namespace dxvk {
     }} },
     /* Warhammer: Online                         */
     { R"(\\WAR(-64)?\.exe$)", {{
-      { "d3d9.allowImplicitDiscard",        "False" },
       { "d3d9.customVendorId",              "1002" },
     }} },
     /* Dragon Nest                               */
