d598fd3156623226f68f314e5e7c04d8fd330d8e
Robin Kertels <robin.kertels@gmail.com>
2022-03-11 14:10:27 +0100

    [d3d9] Add HUD item for unmappable memory

--- src/d3d9/d3d9_hud.h
+++ src/d3d9/d3d9_hud.h
@@ -6,7 +6,7 @@
 namespace dxvk::hud {
 
   /**
-   * \brief HUD item to display DXVK version
+   * \brief HUD item to display sampler count
    */
   class HudSamplerCount : public HudItem {
 
@@ -27,5 +27,37 @@ namespace dxvk::hud {
     std::string m_samplerCount;
 
   };
+
+    /**
+     * \brief HUD item to display unmappable memory
+     */
+    class HudTextureMemory : public HudItem {
+      constexpr static int64_t UpdateInterval = 500'000;
+
+    public:
+
+        HudTextureMemory(D3D9DeviceEx* device);
+
+        void update(dxvk::high_resolution_clock::time_point time);
+
+        HudPos render(
+                HudRenderer&      renderer,
+                HudPos            position);
+
+    private:
+
+        D3D9DeviceEx* m_device;
+
+        uint32_t m_maxAllocated = 0;
+        uint32_t m_maxUsed      = 0;
+        uint32_t m_maxMapped    = 0;
+
+        dxvk::high_resolution_clock::time_point m_lastUpdate
+          = dxvk::high_resolution_clock::now();
+
+        std::string m_allocatedString;
+        std::string m_mappedString;
+
+    };
 
 }
