807dd97abdb1d0ef2466b9b559b46b78e2848ad8
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2022-03-11 12:48:05 +0100

    [dxso] Fix ExpP instruction on Shader Model 2+

--- src/dxso/dxso_compiler.cpp
+++ src/dxso/dxso_compiler.cpp
@@ -2043,9 +2043,9 @@ namespace dxvk {
             result.id = resultIndices[0];
           else
             result.id = m_module.opCompositeConstruct(typeId, result.type.ccount, resultIndices.data());
-
+          break;
         }
-        break;
+        [[fallthrough]];
       case DxsoOpcode::Exp:
         result.id = m_module.opExp2(typeId,
           emitRegisterLoad(src[0], mask).id);
