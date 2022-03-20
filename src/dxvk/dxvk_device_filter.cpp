2a6bf03359c5255718630323e0a26b508a199d1c
Sam Cooper <rukadare@gmail.com>
2022-03-20 22:13:36 +0000

    Correct conversion to uint32_t

--- src/dxvk/dxvk_device_filter.cpp
+++ src/dxvk/dxvk_device_filter.cpp
@@ -1,14 +1,11 @@
 #include "dxvk_device_filter.h"
 
-std::string convertUUID(const uint8_t* uuid) {
-  std::string uuidStr{VK_UUID_SIZE};
-
-  for(unsigned int i = 0; i < VK_UUID_SIZE; i++)
-  {
-    uuidStr[i] = uuid[i];
+std::string convertUUID(const uint8_t uuid[VK_UUID_SIZE]) {
+  std::ostringstream stream;
+  for (unsigned int i = 0; i < VK_UUID_SIZE; i++) {
+    stream << static_cast<uint32_t>(uuid[i]);
   }
-
-  return uuidStr;
+  return stream.str();
 }
 
 namespace dxvk {
