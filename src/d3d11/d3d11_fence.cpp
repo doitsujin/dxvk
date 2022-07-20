73b27a7eabc2324a77daae23b13c27fd6fa15533
Derek Lesho <dlesho@codeweavers.com>
2022-07-19 22:28:26 -0400

    [d3d11] Add support for shared ID3D11Fence resources

--- src/d3d11/d3d11_fence.cpp
+++ src/d3d11/d3d11_fence.cpp
@@ -6,12 +6,21 @@ namespace dxvk {
   D3D11Fence::D3D11Fence(
           D3D11Device*        pDevice,
           UINT64              InitialValue,
-          D3D11_FENCE_FLAG    Flags)
+          D3D11_FENCE_FLAG    Flags,
+          HANDLE              hFence)
   : D3D11DeviceChild<ID3D11Fence>(pDevice) {
     DxvkFenceCreateInfo fenceInfo;
     fenceInfo.initialValue = InitialValue;
+    m_flags = Flags;
 
-    if (Flags)
+    if (Flags & D3D11_FENCE_FLAG_SHARED) {
+      fenceInfo.sharedType = VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_D3D11_FENCE_BIT;
+      if (hFence == nullptr)
+        hFence = INVALID_HANDLE_VALUE;
+      fenceInfo.sharedHandle = hFence;
+    }
+
+    if (Flags & ~D3D11_FENCE_FLAG_SHARED)
       Logger::err(str::format("Fence flags 0x", std::hex, Flags, " not supported"));
 
     m_fence = pDevice->GetDXVKDevice()->createFence(fenceInfo);
@@ -49,14 +58,31 @@ namespace dxvk {
           DWORD               dwAccess,
           LPCWSTR             lpName,
           HANDLE*             pHandle) {
-    Logger::err("D3D11Fence::CreateSharedHandle: Not implemented");
-    return E_NOTIMPL;
+    if (!(m_flags & D3D11_FENCE_FLAG_SHARED))
+      return E_INVALIDARG;
+
+    if (pAttributes)
+      Logger::warn(str::format("CreateSharedHandle: attributes ", pAttributes, " not handled"));
+    if (dwAccess)
+      Logger::warn(str::format("CreateSharedHandle: access ", dwAccess, " not handled"));
+    if (lpName)
+      Logger::warn(str::format("CreateSharedHandle: name ", dxvk::str::fromws(lpName), " not handled"));
+
+    HANDLE sharedHandle = m_fence->sharedHandle();
+    if (sharedHandle == INVALID_HANDLE_VALUE)
+      return E_INVALIDARG;
+
+    *pHandle = sharedHandle;
+    return S_OK;
   }
 
 
   HRESULT STDMETHODCALLTYPE D3D11Fence::SetEventOnCompletion(
           UINT64              Value,
           HANDLE              hEvent) {
+    // TODO in case of rewinds, the stored value may be higher.
+    // For shared fences, calling vkWaitSemaphores here could alleviate the issue.
+
     m_fence->enqueueWait(Value, [hEvent] {
       SetEvent(hEvent);
     });
@@ -66,6 +92,9 @@ namespace dxvk {
 
 
   UINT64 STDMETHODCALLTYPE D3D11Fence::GetCompletedValue() {
+    // TODO in the case of rewinds, the stored value may be higher.
+    // For shared fences, calling vkGetSemaphoreCounterValue here could alleviate the issue.
+
     return m_fence->getValue();
   }
   
