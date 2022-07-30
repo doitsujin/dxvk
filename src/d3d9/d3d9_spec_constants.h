1d1f0c7e7eb41dbbfa44a208bec84c81f5d0b1ea
Joshua Ashton <joshua@froggi.es>
2022-07-30 12:30:33 +0000

    [d3d9] Document D3D9SpecConstantId

--- src/d3d9/d3d9_spec_constants.h
+++ src/d3d9/d3d9_spec_constants.h
@@ -5,21 +5,26 @@
 namespace dxvk {
 
   enum D3D9SpecConstantId : uint32_t {
-    AlphaCompareOp  = 0,
-    SamplerType     = 1,
-    FogEnabled      = 2,
-    VertexFogMode   = 3,
-    PixelFogMode    = 4,
+    AlphaCompareOp  = 0,    // Range: 0 -> 7          | Bits: 3
+    SamplerType     = 1,    // 2 bits for 16 samplers | Bits: 32
+                            // ^ not used for vertex shaders
+    FogEnabled      = 2,    // Range: 0 -> 1          | Bits: 1
+    VertexFogMode   = 3,    // Range: 0 -> 3          | Bits: 2
+    PixelFogMode    = 4,    // Range: 0 -> 3          | Bits: 2
 
-    PointMode       = 5,
-    ProjectionType  = 6,
+    PointMode       = 5,    // Range: 0 -> 3          | Bits: 3
+    ProjectionType  = 6,    // 1 bit for 16 samplers  | Bits: 16
+                            // ^ not supported for vertex shaders
 
-    VertexShaderBools = 7,
-    PixelShaderBools  = 8,
-    Fetch4            = 9,
+    VertexShaderBools = 7,  // 16 bools               | Bits: 16
+    PixelShaderBools  = 8,  // 16 bools               | Bits: 16
+    Fetch4            = 9,  // 1 bit for 16 samplers  | Bits: 16
+                            // ^ not supported for vertex shaders
 
-    SamplerDepthMode  = 10,
-    SamplerNull       = 11,
+    SamplerDepthMode  = 10, // 1 bit for 20 samplers  | Bits: 20
+                            // ^ vs + ps
+    SamplerNull       = 11, // 1 bit for 20 samplers  | Bits: 20
+                            // ^ vs + ps
   };
 
 }
