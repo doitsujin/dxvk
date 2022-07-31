b30dbfc26faec2df442c502f352529cc963fea13
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2022-07-31 18:06:44 +0200

    [dxvk] Disable graphics pipeline libraries on 32-bit builds
    Eats too much address space unfortunately.

--- src/dxvk/dxvk_options.cpp
+++ src/dxvk/dxvk_options.cpp
@@ -1,12 +1,15 @@
 #include "dxvk_options.h"
 
+#include "../util/util_env.h"
+
 namespace dxvk {
 
   DxvkOptions::DxvkOptions(const Config& config) {
     enableDebugUtils      = config.getOption<bool>    ("dxvk.enableDebugUtils",       false);
     enableStateCache      = config.getOption<bool>    ("dxvk.enableStateCache",       true);
     numCompilerThreads    = config.getOption<int32_t> ("dxvk.numCompilerThreads",     0);
-    enableGraphicsPipelineLibrary = config.getOption<Tristate>("dxvk.enableGraphicsPipelineLibrary", Tristate::Auto);
+    enableGraphicsPipelineLibrary = config.getOption<Tristate>("dxvk.enableGraphicsPipelineLibrary",
+      env::is32BitHostPlatform() ? Tristate::False : Tristate::Auto);
     useRawSsbo            = config.getOption<Tristate>("dxvk.useRawSsbo",             Tristate::Auto);
     shrinkNvidiaHvvHeap   = config.getOption<bool>    ("dxvk.shrinkNvidiaHvvHeap",    false);
     hud                   = config.getOption<std::string>("dxvk.hud", "");
