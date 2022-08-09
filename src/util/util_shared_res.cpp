6cf3d808ebfd64071286f0816ed932da95a71947
Joshua Ashton <joshua@froggi.es>
2022-08-09 12:05:47 +0100

    [util] Add stubs for shared resources on other platforms

--- src/util/util_shared_res.cpp
+++ src/util/util_shared_res.cpp
@@ -1,9 +1,13 @@
 #include "util_shared_res.h"
+#include "log/log.h"
 
-#include "winioctl.h"
+#ifdef _WIN32
+#include <winioctl.h>
+#endif
 
 namespace dxvk {
 
+#ifdef _WIN32
   #define IOCTL_SHARED_GPU_RESOURCE_OPEN             CTL_CODE(FILE_DEVICE_VIDEO, 1, METHOD_BUFFERED, FILE_WRITE_ACCESS)
 
   HANDLE openKmtHandle(HANDLE kmt_handle) {
@@ -42,5 +46,21 @@ namespace dxvk {
       *metadataSize = retSize;
     return ret;
   }
+#else
+  HANDLE openKmtHandle(HANDLE kmt_handle) {
+    Logger::warn("openKmtHandle: Shared resources not available on this platform.");
+    return INVALID_HANDLE_VALUE;
+  }
+
+  bool setSharedMetadata(HANDLE handle, void *buf, uint32_t bufSize) {
+    Logger::warn("setSharedMetadata: Shared resources not available on this platform.");
+    return false;
+  }
+
+  bool getSharedMetadata(HANDLE handle, void *buf, uint32_t bufSize, uint32_t *metadataSize) {
+    Logger::warn("getSharedMetadata: Shared resources not available on this platform.");
+    return false;
+  }
+#endif
 
 }
