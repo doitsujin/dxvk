9684a1f2d4c0145c3550bfcb0c228c6bbca4f380
Joshua Ashton <joshua@froggi.es>
2022-08-20 23:43:18 +0000

    [util] Rename always_inline to force_inline
    This conflicts with other libraries using the always_inline attribute in GCC as it defines it with the same name.

--- src/util/util_likely.h
+++ src/util/util_likely.h
@@ -3,9 +3,9 @@
 #ifdef __GNUC__
 #define likely(x)   __builtin_expect((x),1)
 #define unlikely(x) __builtin_expect((x),0)
-#define always_inline inline __attribute__((always_inline))
+#define force_inline inline __attribute__((always_inline))
 #else
 #define likely(x)   (x)
 #define unlikely(x) (x)
-#define always_inline inline
+#define force_inline inline
 #endif
