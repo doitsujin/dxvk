91a5123ad0b254a863885fbee35b85151462d3ee
Joshua Ashton <joshua@froggi.es>
2022-08-20 23:11:36 +0000

    [native] Add SDL2 native headers
    Not included by DXVK at all, but used by external applications.

---
+++ include/native/wsi/native_sdl2.h
@@ -0,0 +1,25 @@
+#include <windows.h>
+
+#include <SDL2/SDL.h>
+
+namespace dxvk::wsi {
+
+  inline SDL_Window* fromHwnd(HWND hWindow) {
+    return reinterpret_cast<SDL_Window*>(hWindow);
+  }
+
+  inline HWND toHwnd(SDL_Window* pWindow) {
+    return reinterpret_cast<HWND>(pWindow);
+  }
+
+  // Offset so null HMONITORs go to -1
+  inline int32_t fromHmonitor(HMONITOR hMonitor) {
+    return static_cast<int32_t>(reinterpret_cast<intptr_t>(hMonitor)) - 1;
+  }
+
+  // Offset so -1 display id goes to 0 == NULL
+  inline HMONITOR toHmonitor(int32_t displayId) {
+    return reinterpret_cast<HMONITOR>(static_cast<intptr_t>(displayId + 1));
+  }
+
+}
