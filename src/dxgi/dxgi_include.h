f0f4258be360ebe237acf2881e3f73401c689e7c
Joshua Ashton <joshua@froggi.es>
2022-08-21 17:39:22 +0000

    [dxgi] Correct DLLEXPORT for native builds

--- src/dxgi/dxgi_include.h
+++ src/dxgi/dxgi_include.h
@@ -1,7 +1,7 @@
 #pragma once
 
 //for some reason we need to specify __declspec(dllexport) for MinGW
-#if defined(__WINE__)
+#if defined(__WINE__) || !defined(_WIN32)
 #define DLLEXPORT __attribute__((visibility("default")))
 #else
 #define DLLEXPORT
