ca05468323f8c8de149dcc3d745c520b6b18d6b9
Joshua Ashton <joshua@froggi.es>
2022-08-21 19:10:05 +0000

    [d3d9] Use topath helpers in shader code

--- src/d3d9/d3d9_fixed_function.cpp
+++ src/d3d9/d3d9_fixed_function.cpp
@@ -2441,7 +2441,7 @@ namespace dxvk {
 
     if (dumpPath.size() != 0) {
       std::ofstream dumpStream(
-        str::tows(str::format(dumpPath, "/", Name, ".spv").c_str()).c_str(),
+        str::topath(str::format(dumpPath, "/", Name, ".spv").c_str()).c_str(),
         std::ios_base::binary | std::ios_base::trunc);
       
       m_shader->dump(dumpStream);
