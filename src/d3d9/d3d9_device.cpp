8e64db95e2f47fdf3924c3d2ff8cfa41a74e2d99
Krzysztof Dobrowolski <krzysztof.dobrowolski@intel.com>
2022-08-24 11:29:16 +0200

    [d3d9] Fix for missing restriction check in UpdateSurface.
    The spec of IDirect3DDevice9::UpdateSurface contains the following restriction:
    - Neither surface can be created with multisampling.
      The only valid flag for both surfaces is D3DMULTISAMPLE_NONE.
    
    This commit adds this check and returns D3DERR_INVALIDCALL
    when source or destination surfaces are multisampled.

--- src/d3d9/d3d9_device.cpp
+++ src/d3d9/d3d9_device.cpp
@@ -746,6 +746,12 @@ namespace dxvk {
     if (unlikely(srcTextureInfo->Desc()->Format != dstTextureInfo->Desc()->Format))
       return D3DERR_INVALIDCALL;
 
+    if (unlikely(srcTextureInfo->Desc()->MultiSample != D3DMULTISAMPLE_NONE))
+      return D3DERR_INVALIDCALL;
+
+    if (unlikely(dstTextureInfo->Desc()->MultiSample != D3DMULTISAMPLE_NONE))
+      return D3DERR_INVALIDCALL;
+
     const DxvkFormatInfo* formatInfo = lookupFormatInfo(dstTextureInfo->GetFormatMapping().FormatColor);
 
     VkOffset3D srcOffset = { 0u, 0u, 0u };
