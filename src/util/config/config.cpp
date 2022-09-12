82059e51197afe36a985f8096b63b6385db3069f
Blisto91 <47954800+Blisto91@users.noreply.github.com>
2022-09-12 17:45:26 +0200

    [util] Enable memoryTrackTest for SiN Episodes Emergence

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -607,6 +607,10 @@ namespace dxvk {
     { R"(\\ship\.exe$)", {{
       { "d3d9.memoryTrackTest",             "True" },
     }} },
+    /* SiN Episodes Emergence                   */
+    { R"(\\SinEpisodes\.exe$)", {{
+      { "d3d9.memoryTrackTest",             "True" },
+    }} },
   }};
 
 
