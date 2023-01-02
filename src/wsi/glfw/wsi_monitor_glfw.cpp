db0f6e9ea535e63f2ed092e7b960c1522071bcc9
Beyley Thomas <ep1cm1n10n123@gmail.com>
2023-01-02 13:36:29 -0800

    Fix formatting more

--- src/wsi/glfw/wsi_monitor_glfw.cpp
+++ src/wsi/glfw/wsi_monitor_glfw.cpp
@@ -11,150 +11,150 @@
 
 namespace dxvk::wsi {
 
-	HMONITOR getDefaultMonitor() {
-		return enumMonitors(0);
-	}
+  HMONITOR getDefaultMonitor() {
+    return enumMonitors(0);
+  }
 
 
-	HMONITOR enumMonitors(uint32_t index) {
-		return isDisplayValid(int32_t(index))
-			   ? toHmonitor(index)
-			   : nullptr;
-	}
+  HMONITOR enumMonitors(uint32_t index) {
+    return isDisplayValid(int32_t(index))
+         ? toHmonitor(index)
+         : nullptr;
+  }
 
-	bool getDisplayName(
-			HMONITOR hMonitor,
-			WCHAR            (&Name)[32]) {
-		const int32_t displayId = fromHmonitor(hMonitor);
+  bool getDisplayName(
+      HMONITOR hMonitor,
+      WCHAR            (&Name)[32]) {
+    const int32_t displayId = fromHmonitor(hMonitor);
 
-		if (!isDisplayValid(displayId))
-			return false;
+    if (!isDisplayValid(displayId))
+      return false;
 
-		std::wstringstream nameStream;
-		nameStream << LR"(\\.\DISPLAY)" << (displayId + 1);
+    std::wstringstream nameStream;
+    nameStream << LR"(\\.\DISPLAY)" << (displayId + 1);
 
-		std::wstring name = nameStream.str();
+    std::wstring name = nameStream.str();
 
-		std::memset(Name, 0, sizeof(Name));
-		name.copy(Name, name.length(), 0);
+    std::memset(Name, 0, sizeof(Name));
+    name.copy(Name, name.length(), 0);
 
-		return true;
-	}
-
-
-	bool getDesktopCoordinates(
-			HMONITOR hMonitor,
-			RECT* pRect) {
-		const int32_t displayId = fromHmonitor(hMonitor);
-
-		if (!isDisplayValid(displayId))
-			return false;
-
-		int32_t displayCount = 0;
-		GLFWmonitor** monitors = glfwGetMonitors(&displayCount);
-		GLFWmonitor* monitor = monitors[displayId];
-
-		int32_t x;
-		int32_t y;
-		int32_t w;
-		int32_t h;
-		glfwGetMonitorWorkarea(monitor, &x, &y, &w, &h);
-
-		pRect->left = x;
-		pRect->top = y;
-		pRect->right = x + w;
-		pRect->bottom = y + h;
-
-		return true;
-	}
-
-	static inline uint32_t roundToNextPow2(uint32_t num) {
-		if (num-- == 0)
-			return 0;
-
-		num |= num >> 1;
-		num |= num >> 2;
-		num |= num >> 4;
-		num |= num >> 8;
-		num |= num >> 16;
-
-		return ++num;
-	}
+    return true;
+  }
 
 
-	static inline void convertMode(const GLFWvidmode& mode, WsiMode* pMode) {
-		pMode->width = uint32_t(mode.width);
-		pMode->height = uint32_t(mode.height);
-		pMode->refreshRate = WsiRational{uint32_t(mode.refreshRate) * 1000, 1000};
-		// BPP should always be a power of two
-		// to match Windows behaviour of including padding.
-		pMode->bitsPerPixel = roundToNextPow2(mode.blueBits + mode.redBits + mode.greenBits);
-		pMode->interlaced = false;
-	}
+  bool getDesktopCoordinates(
+      HMONITOR hMonitor,
+      RECT* pRect) {
+    const int32_t displayId = fromHmonitor(hMonitor);
+
+    if (!isDisplayValid(displayId))
+      return false;
+
+    int32_t displayCount = 0;
+    GLFWmonitor** monitors = glfwGetMonitors(&displayCount);
+    GLFWmonitor* monitor = monitors[displayId];
+
+    int32_t x;
+    int32_t y;
+    int32_t w;
+    int32_t h;
+    glfwGetMonitorWorkarea(monitor, &x, &y, &w, &h);
+
+    pRect->left = x;
+    pRect->top = y;
+    pRect->right = x + w;
+    pRect->bottom = y + h;
+
+    return true;
+  }
+
+  static inline uint32_t roundToNextPow2(uint32_t num) {
+    if (num-- == 0)
+      return 0;
+
+    num |= num >> 1;
+    num |= num >> 2;
+    num |= num >> 4;
+    num |= num >> 8;
+    num |= num >> 16;
+
+    return ++num;
+  }
 
 
-	bool getDisplayMode(
-			HMONITOR hMonitor,
-			uint32_t ModeNumber,
-			WsiMode* pMode) {
-		const int32_t displayId = fromHmonitor(hMonitor);
-		int32_t displayCount = 0;
-		GLFWmonitor** monitors = glfwGetMonitors(&displayCount);
-		GLFWmonitor* monitor = monitors[displayId];
-
-		if (!isDisplayValid(displayId))
-			return false;
-
-		int32_t count = 0;
-		const GLFWvidmode* modes = glfwGetVideoModes(monitor, &count);
-
-		convertMode(modes[ModeNumber], pMode);
-
-		return true;
-	}
+  static inline void convertMode(const GLFWvidmode& mode, WsiMode* pMode) {
+    pMode->width = uint32_t(mode.width);
+    pMode->height = uint32_t(mode.height);
+    pMode->refreshRate = WsiRational{uint32_t(mode.refreshRate) * 1000, 1000};
+    // BPP should always be a power of two
+    // to match Windows behaviour of including padding.
+    pMode->bitsPerPixel = roundToNextPow2(mode.blueBits + mode.redBits + mode.greenBits);
+    pMode->interlaced = false;
+  }
 
 
-	bool getCurrentDisplayMode(
-			HMONITOR hMonitor,
-			WsiMode* pMode) {
-		const int32_t displayId = fromHmonitor(hMonitor);
+  bool getDisplayMode(
+      HMONITOR hMonitor,
+      uint32_t ModeNumber,
+      WsiMode* pMode) {
+    const int32_t displayId = fromHmonitor(hMonitor);
+    int32_t displayCount = 0;
+    GLFWmonitor** monitors = glfwGetMonitors(&displayCount);
+    GLFWmonitor* monitor = monitors[displayId];
 
-		if (!isDisplayValid(displayId))
-			return false;
+    if (!isDisplayValid(displayId))
+      return false;
 
-		int32_t displayCount = 0;
-		GLFWmonitor** monitors = glfwGetMonitors(&displayCount);
-		GLFWmonitor* monitor = monitors[displayId];
+    int32_t count = 0;
+    const GLFWvidmode* modes = glfwGetVideoModes(monitor, &count);
 
-		auto mode = glfwGetVideoMode(monitor);
+    convertMode(modes[ModeNumber], pMode);
 
-		convertMode(*mode, pMode);
-
-		return true;
-	}
+    return true;
+  }
 
 
-	bool getDesktopDisplayMode(
-			HMONITOR hMonitor,
-			WsiMode* pMode) {
-		const int32_t displayId = fromHmonitor(hMonitor);
+  bool getCurrentDisplayMode(
+      HMONITOR hMonitor,
+      WsiMode* pMode) {
+    const int32_t displayId = fromHmonitor(hMonitor);
 
-		if (!isDisplayValid(displayId))
-			return false;
+    if (!isDisplayValid(displayId))
+      return false;
 
-		int32_t displayCount = 0;
-		GLFWmonitor** monitors = glfwGetMonitors(&displayCount);
-		GLFWmonitor* monitor = monitors[displayId];
+    int32_t displayCount = 0;
+    GLFWmonitor** monitors = glfwGetMonitors(&displayCount);
+    GLFWmonitor* monitor = monitors[displayId];
 
-		//TODO: actually implement this properly, currently we just grab the current one
-		convertMode(*glfwGetVideoMode(monitor), pMode);
+    auto mode = glfwGetVideoMode(monitor);
 
-		return true;
-	}
+    convertMode(*mode, pMode);
 
-	std::vector<uint8_t> getMonitorEdid(HMONITOR hMonitor) {
-		Logger::err("getMonitorEdid not implemented on this platform.");
-		return {};
-	}
+    return true;
+  }
+
+
+  bool getDesktopDisplayMode(
+      HMONITOR hMonitor,
+      WsiMode* pMode) {
+    const int32_t displayId = fromHmonitor(hMonitor);
+
+    if (!isDisplayValid(displayId))
+      return false;
+
+    int32_t displayCount = 0;
+    GLFWmonitor** monitors = glfwGetMonitors(&displayCount);
+    GLFWmonitor* monitor = monitors[displayId];
+
+    //TODO: actually implement this properly, currently we just grab the current one
+    convertMode(*glfwGetVideoMode(monitor), pMode);
+
+    return true;
+  }
+
+  std::vector<uint8_t> getMonitorEdid(HMONITOR hMonitor) {
+    Logger::err("getMonitorEdid not implemented on this platform.");
+    return {};
+  }
 
 }
--- src/wsi/glfw/wsi_window_glfw.cpp
+++ src/wsi/glfw/wsi_window_glfw.cpp
@@ -12,133 +12,133 @@
 
 namespace dxvk::wsi {
 
-	void getWindowSize(
-			HWND hWindow,
-			uint32_t* pWidth,
-			uint32_t* pHeight) {
-		GLFWwindow* window = fromHwnd(hWindow);
+  void getWindowSize(
+      HWND hWindow,
+      uint32_t* pWidth,
+      uint32_t* pHeight) {
+    GLFWwindow* window = fromHwnd(hWindow);
 
-		int32_t w, h;
-		glfwGetWindowSize(window, &w, &h);
+    int32_t w, h;
+    glfwGetWindowSize(window, &w, &h);
 
-		if (pWidth)
-			*pWidth = uint32_t(w);
+    if (pWidth)
+      *pWidth = uint32_t(w);
 
-		if (pHeight)
-			*pHeight = uint32_t(h);
-	}
-
-
-	void resizeWindow(
-			HWND hWindow,
-			DxvkWindowState* pState,
-			uint32_t Width,
-			uint32_t Height) {
-		GLFWwindow* window = fromHwnd(hWindow);
-
-		glfwSetWindowSize(window, int32_t(Width), int32_t(Height));
-	}
+    if (pHeight)
+      *pHeight = uint32_t(h);
+  }
 
 
-	bool setWindowMode(
-			HMONITOR hMonitor,
-			HWND hWindow,
-			const WsiMode& pMode) {
-		const int32_t displayId = fromHmonitor(hMonitor);
-		GLFWwindow* window = fromHwnd(hWindow);
+  void resizeWindow(
+      HWND hWindow,
+      DxvkWindowState* pState,
+      uint32_t Width,
+      uint32_t Height) {
+    GLFWwindow* window = fromHwnd(hWindow);
 
-		if (!isDisplayValid(displayId))
-			return false;
-
-		int32_t displayCount = 0;
-		GLFWmonitor** monitors = glfwGetMonitors(&displayCount);
-		GLFWmonitor* monitor = monitors[displayId];
-
-		GLFWvidmode wantedMode = {};
-		wantedMode.width = pMode.width;
-		wantedMode.height = pMode.height;
-		wantedMode.refreshRate = pMode.refreshRate.numerator != 0
-								 ? pMode.refreshRate.numerator / pMode.refreshRate.denominator
-								 : 0;
-		// TODO: Implement lookup format for bitsPerPixel here.
-
-		glfwSetWindowMonitor(window, monitor, 0, 0, wantedMode.width, wantedMode.width, wantedMode.refreshRate);
-
-		return true;
-	}
-
-	bool enterFullscreenMode(
-			HMONITOR hMonitor,
-			HWND hWindow,
-			DxvkWindowState* pState,
-			bool ModeSwitch) {
-		const int32_t displayId = fromHmonitor(hMonitor);
-		GLFWwindow* window = fromHwnd(hWindow);
-
-		if (!isDisplayValid(displayId))
-			return false;
-
-		GLFWmonitor* monitor = glfwGetPrimaryMonitor();
-		auto videoMode = glfwGetVideoMode(monitor);
-
-		// TODO: Set this on the correct monitor.
-		// Docs aren't clear on this...
-		glfwSetWindowMonitor(window, monitor, 0, 0, videoMode->width, videoMode->height, videoMode->refreshRate);
-
-		return true;
-	}
+    glfwSetWindowSize(window, int32_t(Width), int32_t(Height));
+  }
 
 
-	bool leaveFullscreenMode(
-			HWND hWindow,
-			DxvkWindowState* pState,
-			bool             restoreCoordinates) {
-		GLFWwindow* window = fromHwnd(hWindow);
+  bool setWindowMode(
+      HMONITOR hMonitor,
+      HWND hWindow,
+      const WsiMode& pMode) {
+    const int32_t displayId = fromHmonitor(hMonitor);
+    GLFWwindow* window = fromHwnd(hWindow);
 
-		GLFWmonitor* monitor = glfwGetPrimaryMonitor();
-		auto videoMode = glfwGetVideoMode(monitor);
-		glfwSetWindowMonitor(window, nullptr, 0, 0, videoMode->width, videoMode->height, videoMode->refreshRate);
+    if (!isDisplayValid(displayId))
+      return false;
 
-		return true;
-	}
+    int32_t displayCount = 0;
+    GLFWmonitor** monitors = glfwGetMonitors(&displayCount);
+    GLFWmonitor* monitor = monitors[displayId];
+
+    GLFWvidmode wantedMode = {};
+    wantedMode.width = pMode.width;
+    wantedMode.height = pMode.height;
+    wantedMode.refreshRate = pMode.refreshRate.numerator != 0
+                 ? pMode.refreshRate.numerator / pMode.refreshRate.denominator
+                 : 0;
+    // TODO: Implement lookup format for bitsPerPixel here.
+
+    glfwSetWindowMonitor(window, monitor, 0, 0, wantedMode.width, wantedMode.width, wantedMode.refreshRate);
+
+    return true;
+  }
+
+  bool enterFullscreenMode(
+      HMONITOR hMonitor,
+      HWND hWindow,
+      DxvkWindowState* pState,
+      bool ModeSwitch) {
+    const int32_t displayId = fromHmonitor(hMonitor);
+    GLFWwindow* window = fromHwnd(hWindow);
+
+    if (!isDisplayValid(displayId))
+      return false;
+
+    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+    auto videoMode = glfwGetVideoMode(monitor);
+
+    // TODO: Set this on the correct monitor.
+    // Docs aren't clear on this...
+    glfwSetWindowMonitor(window, monitor, 0, 0, videoMode->width, videoMode->height, videoMode->refreshRate);
+
+    return true;
+  }
 
 
-	bool restoreDisplayMode() {
-		// Don't need to do anything with GLFW here.
-		return true;
-	}
+  bool leaveFullscreenMode(
+      HWND hWindow,
+      DxvkWindowState* pState,
+      bool             restoreCoordinates) {
+    GLFWwindow* window = fromHwnd(hWindow);
+
+    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+    auto videoMode = glfwGetVideoMode(monitor);
+    glfwSetWindowMonitor(window, nullptr, 0, 0, videoMode->width, videoMode->height, videoMode->refreshRate);
+
+    return true;
+  }
 
 
-	HMONITOR getWindowMonitor(HWND hWindow) {
-		// TODO: implement this with glfwGetWindowMonitor 
-		//  (or maybe not? glfwGetWindowMonitor only seems to reference *fullscreen* windows)
-		// GLFWwindow* window = fromHwnd(hWindow);
-		const int32_t displayId = 0;
-
-		return toHmonitor(displayId);
-	}
+  bool restoreDisplayMode() {
+    // Don't need to do anything with GLFW here.
+    return true;
+  }
 
 
-	bool isWindow(HWND hWindow) {
-		GLFWwindow* window = fromHwnd(hWindow);
-		return window != nullptr;
-	}
+  HMONITOR getWindowMonitor(HWND hWindow) {
+    // TODO: implement this with glfwGetWindowMonitor 
+    //  (or maybe not? glfwGetWindowMonitor only seems to reference *fullscreen* windows)
+    // GLFWwindow* window = fromHwnd(hWindow);
+    const int32_t displayId = 0;
 
-	void updateFullscreenWindow(
-			HMONITOR hMonitor,
-			HWND     hWindow,
-			bool     forceTopmost) {
-		// Don't need to do anything with GLFW here.
-	}
+    return toHmonitor(displayId);
+  }
 
-	VkResult createSurface(
-			HWND hWindow,
-			PFN_vkGetInstanceProcAddr pfnVkGetInstanceProcAddr,
-			VkInstance                instance,
-			VkSurfaceKHR* pSurface) {
-		GLFWwindow* window = fromHwnd(hWindow);
 
-		return glfwCreateWindowSurface(instance, window, nullptr, pSurface);
-	}
+  bool isWindow(HWND hWindow) {
+    GLFWwindow* window = fromHwnd(hWindow);
+    return window != nullptr;
+  }
+
+  void updateFullscreenWindow(
+      HMONITOR hMonitor,
+      HWND     hWindow,
+      bool     forceTopmost) {
+    // Don't need to do anything with GLFW here.
+  }
+
+  VkResult createSurface(
+      HWND hWindow,
+      PFN_vkGetInstanceProcAddr pfnVkGetInstanceProcAddr,
+      VkInstance                instance,
+      VkSurfaceKHR* pSurface) {
+    GLFWwindow* window = fromHwnd(hWindow);
+
+    return glfwCreateWindowSurface(instance, window, nullptr, pSurface);
+  }
 
 }
