dbe4e86bcdcc08ae28f88cba25e4102448bae419
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2023-01-18 17:29:21 +0100

    [d3d11] Only store low 8 bits of stencil reference
    Seems to match behaviour of the D3D11 runtime, in that OMGetDepthStencilState
    will not retain the high bits.
    
    Found by CME. Should fix #1784.

--- src/d3d11/d3d11_context.cpp
+++ src/d3d11/d3d11_context.cpp
@@ -2525,7 +2525,11 @@ namespace dxvk {
       m_state.om.dsState = depthStencilState;
       ApplyDepthStencilState();
     }
-    
+
+    // The D3D11 runtime only appears to store the low 8 bits,
+    // and some games rely on this behaviour. Do the same here.
+    StencilRef &= 0xFF;
+
     if (m_state.om.stencilRef != StencilRef) {
       m_state.om.stencilRef = StencilRef;
       ApplyStencilRef();
