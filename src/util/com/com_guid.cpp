aa92cf48f557bb9876672d2513cdac5c78941ade
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2023-03-01 13:01:48 +0100

    [util] Add function to cache QueryInterface errors

--- src/util/com/com_guid.cpp
+++ src/util/com/com_guid.cpp
@@ -1,8 +1,46 @@
+#include <mutex>
+#include <unordered_set>
+
 #include "com_guid.h"
 
 #include "../../d3d11/d3d11_interfaces.h"
 
 #include "../../dxgi/dxgi_interfaces.h"
+
+#include "../../dxvk/dxvk_hash.h"
+
+#include "../thread.h"
+
+namespace dxvk {
+
+  struct GuidPair {
+    GuidPair() { };
+    GuidPair(IID a_, IID b_)
+    : a(a_), b(b_) { }
+
+    IID a, b;
+
+    size_t hash() const {
+      return size_t(a.Data1) ^ size_t(b.Data1);
+    }
+
+    bool eq(const GuidPair& other) const {
+      return a == other.a && b == other.b;
+    }
+  };
+
+  dxvk::mutex                                    g_loggedQueryInterfaceErrorMutex;
+  std::unordered_set<GuidPair, DxvkHash, DxvkEq> g_loggedQueryInterfaceErrors;
+
+  bool logQueryInterfaceError(REFIID objectGuid, REFIID requestedGuid) {
+    if (Logger::logLevel() > LogLevel::Warn)
+      return false;
+
+    std::lock_guard lock(g_loggedQueryInterfaceErrorMutex);
+    return g_loggedQueryInterfaceErrors.emplace(objectGuid, requestedGuid).second;
+  }
+
+}
 
 std::ostream& operator << (std::ostream& os, REFIID guid) {
   os << std::hex << std::setfill('0')
