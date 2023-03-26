d8779dc64b958852891d4d7576f79bfb66509fba
Winter Snowfall <WinterSnowfall@users.noreply.github.com>
2023-03-26 22:23:16 +0300

    [util] Enable apitraceMode for Motor City Online

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -1025,6 +1025,10 @@ namespace dxvk {
     { R"(\\RF\.exe$)", {{
       { "d3d9.allowDirectBufferMapping",   "False" },
     }} },
+    /* Motor City Online                         */
+    { R"(\\MCity_d\.exe$)", {{
+      { "d3d9.apitraceMode",                "True" },
+    }} },
   }};
 
 
