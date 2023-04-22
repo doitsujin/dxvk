4810374c6f8825634f5d34634e9fe656e9f41583
Alpyne <alpyne.dreams@gmail.com>
2023-04-21 20:30:22 -0700

    [d3d8] Add managedBufferPlacement to dxvk.conf

--- src/d3d8/d3d8_options.h
+++ src/d3d8/d3d8_options.h
@@ -7,14 +7,17 @@
 namespace dxvk {
   struct D3D8Options {
 
-    /// Remap DEFAULT pool vertex buffers to MANAGED in order to optimize
-    /// performance in cases where applications perform read backs
+    /// Remap DEFAULT pool vertex and index buffers to MANAGED to improve
+    /// performance by avoiding waiting for games that frequently lock their buffers.
+    ///
+    /// This implicitly disables direct buffer mapping. Some applications may
+    /// need this option disabled to keep from overwriting in-use buffer regions.
     bool managedBufferPlacement = true;
 
     D3D8Options() {}
     D3D8Options(const Config& config) {
-      useShadowBuffers = config.getOption("d3d8.useShadowBuffers", useShadowBuffers);
-      managedBufferPlacement = config.getOption("d3d8.managedBufferPlacement", managedBufferPlacement);
+      useShadowBuffers        = config.getOption("d3d8.useShadowBuffers",       useShadowBuffers);
+      managedBufferPlacement  = config.getOption("d3d8.managedBufferPlacement", managedBufferPlacement);
     }
   };
 
