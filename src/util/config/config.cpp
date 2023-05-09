79d04c430047f6fc84d9796554f100ce91ce588d
Alpyne <alpyne.dreams@gmail.com>
2023-05-09 00:02:22 -0700

    [d3d8] Use batcher for NSF 3 and 4

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -1061,6 +1061,7 @@ namespace dxvk {
       { "d3d9.enableDialogMode",            "True" },
       { "d3d9.apitraceMode",                "True" },
       { "d3d8.managedBufferPlacement",     "False" },
+      { "d3d8.batching",                    "True" },
     }} },
     /* Need for Speed: High Stakes / Road         *
        Challenge (with the "Modern Patch") -      *
@@ -1071,6 +1072,7 @@ namespace dxvk {
       { "d3d9.memoryTrackTest",             "True" },
       { "d3d9.maxAvailableMemory",           "256" },
       { "d3d8.managedBufferPlacement",     "False" },
+      { "d3d8.batching",                    "True" },
     }} },
   }};
 
