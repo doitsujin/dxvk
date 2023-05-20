4e5238e81bb38d68d63e2c50367091737b68e751
Lilium <19245343+EndlesslyFlowering@users.noreply.github.com>
2023-05-20 14:08:00 +0200

    [d3d11] implement 'clampNegativeLodBias' as a conf option

--- src/d3d11/d3d11_options.cpp
+++ src/d3d11/d3d11_options.cpp
@@ -21,6 +21,7 @@ namespace dxvk {
     this->maxTessFactor         = config.getOption<int32_t>("d3d11.maxTessFactor", 0);
     this->samplerAnisotropy     = config.getOption<int32_t>("d3d11.samplerAnisotropy", -1);
     this->samplerLodBias        = config.getOption<float>("d3d11.samplerLodBias", 0.0f);
+    this->clampNegativeLodBias  = config.getOption<bool>("d3d11.clampNegativeLodBias", false);
     this->invariantPosition     = config.getOption<bool>("d3d11.invariantPosition", true);
     this->floatControls         = config.getOption<bool>("d3d11.floatControls", true);
     this->forceSampleRateShading = config.getOption<bool>("d3d11.forceSampleRateShading", false);
