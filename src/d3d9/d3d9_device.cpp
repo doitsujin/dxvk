doitsujin/dxvk

commit 8560efa3c7c4dbdb8b9fcb6e0aed282b43781c56

[d3d9] Unbind RTs which are unwritten by a PS
Otherwise we can end up with feedback loops on RTs with a 0 color mask.

Closes: #3447

src/d3d9/d3d9_device.cpp
@@ -3278,21 +3278,33 @@ namespace dxvk {
 
     m_state.pixelShader = shader;
 
+    D3D9ShaderMasks newShaderMasks;
+
     if (shader != nullptr) {
       m_flags.set(D3D9DeviceFlag::DirtyFFPixelShader);
 
       BindShader<DxsoProgramTypes::PixelShader>(GetCommonShader(shader));
-      m_psShaderMasks = newShader->GetShaderMask();
+      newShaderMasks = newShader->GetShaderMask();
     }
     else {
       // TODO: What fixed function textures are in use?
       // Currently we are making all 8 of them as in use here.
 
       // The RT output is always 0 for fixed function.
-      m_psShaderMasks = FixedFunctionMask;
+      newShaderMasks = FixedFunctionMask;
     }
 
-    UpdateActiveHazardsRT(UINT32_MAX);
+    // If we have any RTs we would have bound to the the FB
+    // not in the new shader mask, mark the framebuffer as dirty
+    // so we unbind them.
+    if (m_activeRTs & m_psShaderMasks.rtMask & (~newShaderMasks.rtMask))
+      m_flags.set(D3D9DeviceFlag::DirtyFramebuffer);
+
+    if (m_psShaderMasks.samplerMask != newShaderMasks.samplerMask ||
+        m_psShaderMasks.rtMask != newShaderMasks.rtMask) {
+      m_psShaderMasks = newShaderMasks;
+      UpdateActiveHazardsRT(UINT32_MAX);
+    }
 
     return D3D_OK;
   }
@@ -5664,6 +5676,9 @@ namespace dxvk {
         continue;
 
       if (!m_state.renderStates[ColorWriteIndex(i)])
+        continue;
+
+      if (!(m_psShaderMasks.rtMask & (1 << i)))
         continue;
 
       attachments.color[i] = {

commit 6b60de2d310cc1c4351e6423d226aca47eb5e0a2

[d3d9] Fix unbinding RTs
Fixes a regression with 8560efa3c7c4dbdb8b9fcb6e0aed282b43781c56 in ND1.

Also more optimized.

src/d3d9/d3d9_device.cpp
@@ -3394,7 +3394,7 @@ namespace dxvk {
     if (shader != nullptr) {
       m_flags.set(D3D9DeviceFlag::DirtyFFPixelShader);
 
-      BindShader<DxsoProgramTypes::PixelShader>(GetCommonShader(shader));
+      BindShader<DxsoProgramTypes::PixelShader>(newShader);
       newShaderMasks = newShader->GetShaderMask();
     }
     else {
@@ -3408,7 +3408,9 @@ namespace dxvk {
     // If we have any RTs we would have bound to the the FB
     // not in the new shader mask, mark the framebuffer as dirty
     // so we unbind them.
-    if (m_boundRTs & m_anyColorWrites & m_psShaderMasks.rtMask & (~newShaderMasks.rtMask))
+    uint32_t oldUseMask = m_activeRTs & m_psShaderMasks.rtMask;
+    uint32_t newUseMask = m_activeRTs & newShaderMasks.rtMask;
+    if (oldUseMask != newUseMask)
       m_flags.set(D3D9DeviceFlag::DirtyFramebuffer);
 
     if (m_psShaderMasks.samplerMask != newShaderMasks.samplerMask ||