dfcd7aedd8d970ae9a4b1ba1c2c53bc94cc5c8e1
Blisto91 <47954800+Blisto91@users.noreply.github.com>
2023-08-09 12:18:10 +0200

    [util] Limit Conflict Vietnam to 60fps (#3606)
    Physics can bug out at higher fps making the character fly or get stuck when running up or down slopes.

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -795,6 +795,10 @@ namespace dxvk {
     { R"(\\IncPC\.exe$)", {{
       { "d3d9.maxFrameRate",                "59" },
     }} },
+    /* Conflict Vietnam                        */
+    { R"(\\Vietnam\.exe$)", {{
+      { "d3d9.maxFrameRate",                "60" },
+    }} },
     
     /**********************************************/
     /* D3D12 GAMES (vkd3d-proton with dxvk dxgi)  */
