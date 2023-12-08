10b83d184b0b17327336c9078fa9f23498828768
Ethan Lee <flibitijibibo@gmail.com>
2023-12-08 00:59:23 -0500

    [native] Dynamically load SDL2/GLFW at runtime.
    Removing these link-time dependencies is important for making a single binary that is compatible with either backend, regardless of whether or not each one is currently available to the program.

--- src/wsi/sdl2/wsi_platform_sdl2.cpp
+++ src/wsi/sdl2/wsi_platform_sdl2.cpp
@@ -1,10 +1,38 @@
 #include "wsi_platform_sdl2.h"
 #include "../../util/util_error.h"
 #include "../../util/util_string.h"
+#include "../../util/util_win32_compat.h"
 
 #include <SDL2/SDL_vulkan.h>
 
 namespace dxvk::wsi {
+
+  Sdl2WsiDriver::Sdl2WsiDriver() {
+    libsdl = LoadLibraryA( // FIXME: Get soname as string from meson
+#if defined(_WIN32)
+        "SDL2.dll"
+#elif defined(__APPLE__)
+        "libSDL2-2.0.0.dylib"
+#else
+        "libSDL2-2.0.so.0"
+#endif
+      );
+    if (libsdl == nullptr)
+      throw DxvkError("SDL2 WSI: Failed to load SDL2 DLL.");
+
+    #define SDL_PROC(ret, name, params) \
+      name = reinterpret_cast<pfn_##name>(GetProcAddress(libsdl, #name)); \
+      if (name == nullptr) { \
+        FreeLibrary(libsdl); \
+        libsdl = nullptr; \
+        throw DxvkError("SDL2 WSI: Failed to load " #name "."); \
+      }
+    #include "wsi_platform_sdl2_funcs.h"
+  }
+
+  Sdl2WsiDriver::~Sdl2WsiDriver() {
+    FreeLibrary(libsdl);
+  }
 
   std::vector<const char *> Sdl2WsiDriver::getInstanceExtensions() {
     SDL_Vulkan_LoadLibrary(nullptr);
