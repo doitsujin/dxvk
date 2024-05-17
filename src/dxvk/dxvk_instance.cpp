2cb2f8694e4695a8fa28ca9501f1f9a12477b1b6
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2024-05-17 20:39:18 +0200

    [dxvk] Use VK_MAKE_API_VERSION instead of VK_MAKE_VERSION.
    The old macro is deprecated.

--- src/dxvk/dxvk_instance.cpp
+++ src/dxvk/dxvk_instance.cpp
@@ -182,8 +182,8 @@ namespace dxvk {
       appInfo.pApplicationName      = appName.c_str();
       appInfo.applicationVersion    = flags.raw();
       appInfo.pEngineName           = "DXVK";
-      appInfo.engineVersion         = VK_MAKE_VERSION(2, 3, 2);
-      appInfo.apiVersion            = VK_MAKE_VERSION(1, 3, 0);
+      appInfo.engineVersion         = VK_MAKE_API_VERSION(0, 2, 3, 2);
+      appInfo.apiVersion            = VK_MAKE_API_VERSION(0, 1, 3, 0);
 
       VkInstanceCreateInfo info = { VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO };
       info.pApplicationInfo         = &appInfo;
