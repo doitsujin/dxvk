a7b3b1e3a49718d9a3dec166b231f299e12083b2
WinterSnowfall <WinterSnowfall@users.noreply.github.com>
2024-07-13 20:17:09 +0300

    [d3d8] Add an option to respect DISCARD only for dynamic write-only buffers

--- src/d3d8/d3d8_buffer.h
+++ src/d3d8/d3d8_buffer.h
@@ -18,6 +18,7 @@ namespace dxvk {
       : D3D8Resource<D3D9, D3D8> (pDevice, std::move(pBuffer))
       , m_pool                   (Pool)
       , m_usage                  (Usage) {
+      m_options = this->GetParent()->GetOptions();
     }
 
     HRESULT STDMETHODCALLTYPE Lock(
@@ -25,6 +26,13 @@ namespace dxvk {
             UINT   SizeToLock,
             BYTE** ppbData,
             DWORD  Flags) {
+
+      if (m_options->forceLegacyDiscard &&
+          (Flags & D3DLOCK_DISCARD) &&
+         !((m_usage & D3DUSAGE_DYNAMIC) &&
+           (m_usage & D3DUSAGE_WRITEONLY)))
+          Flags &= ~D3DLOCK_DISCARD;
+
       return this->GetD3D9()->Lock(
         OffsetToLock,
         SizeToLock,
@@ -41,8 +49,9 @@ namespace dxvk {
     }
 
   protected:
-    const D3DPOOL m_pool;
-    const DWORD   m_usage;
+    const D3D8Options* m_options;
+    const D3DPOOL      m_pool;
+    const DWORD        m_usage;
   };
 
 
