b74725b264700a57c41ad9f8db22f008b315fc4e
Felix Klinge <f.klinge15@gmail.com>
2024-07-15 21:08:32 +0200

    Add Batman Arkham Knight Custom Vendor ID to config to fix the game for Intel GPUs

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -55,6 +55,11 @@ namespace dxvk {
     /* D3D11 GAMES                                */
     /**********************************************/
 
+    /* Batman Arkham Knight - doesn't like intel vendor id 
+      (refuses to boot if vendor isn't 0x10de or 0x1002)  */
+    { R"(\\BatmanAK\.exe$)", {{
+      { "dxgi.customVendorId",              "10de" },
+    }} },
     /* Assassin's Creed Syndicate: amdags issues  */
     { R"(\\ACS\.exe$)", {{
       { "dxgi.customVendorId",              "10de" },
