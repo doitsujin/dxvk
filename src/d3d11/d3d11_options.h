ff84f07e5b620d63e4c0c28482b4e21dc5cacbd3
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2024-08-08 16:03:16 +0200

    [dxbc,d3d11] Add option to explicitly emit long dot products

--- src/d3d11/d3d11_options.h
+++ src/d3d11/d3d11_options.h
@@ -118,8 +118,11 @@ namespace dxvk {
     /// Shader dump path
     std::string shaderDumpPath;
 
-    /// Should we make our Mads a FFma or do it the long way with an FMul and an FAdd?
+    /// Translate Mad/Dfma to separate FMul+FAdd
     bool longMad;
+
+    /// Translate DpX to a precise FMul+FFma chain
+    bool longDot;
 
     /// Ensure that for the same D3D commands the output VK commands
     /// don't change between runs. Useful for comparative benchmarking,
