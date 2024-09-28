131afa7e57b291612ab5edbe68fd83ff3e355a96
WinterSnowfall <WinterSnowfall@users.noreply.github.com>
2024-09-28 15:53:02 +0300

    [d3d9] Implement a software cursor

--- src/d3d9/d3d9_cursor.cpp
+++ src/d3d9/d3d9_cursor.cpp
@@ -6,6 +6,21 @@
 namespace dxvk {
 
 #ifdef _WIN32
+  void D3D9Cursor::ResetCursor() {
+    ShowCursor(FALSE);
+
+    if (likely(m_hCursor != nullptr)) {
+      ::DestroyCursor(m_hCursor);
+      m_hCursor = nullptr;
+    } else {
+      m_sCursor.Width = 0;
+      m_sCursor.Height = 0;
+      m_sCursor.X = 0;
+      m_sCursor.Y = 0;
+    }
+  }
+
+
   void D3D9Cursor::UpdateCursor(int X, int Y) {
     POINT currentPos = { };
     if (::GetCursorPos(&currentPos) && currentPos == POINT{ X, Y })
@@ -15,17 +30,31 @@ namespace dxvk {
   }
 
 
+  void D3D9Cursor::RefreshSoftwareCursorPosition() {
+    POINT currentPos = { };
+    ::GetCursorPos(&currentPos);
+
+    m_sCursor.X = static_cast<UINT>(currentPos.x);
+    m_sCursor.Y = static_cast<UINT>(currentPos.y);
+  }
+
+
   BOOL D3D9Cursor::ShowCursor(BOOL bShow) {
     if (likely(m_hCursor != nullptr))
       ::SetCursor(bShow ? m_hCursor : nullptr);
-    else
-      Logger::debug("D3D9Cursor::ShowCursor: Software cursor not implemented.");
     
     return std::exchange(m_visible, bShow);
   }
 
 
   HRESULT D3D9Cursor::SetHardwareCursor(UINT XHotSpot, UINT YHotSpot, const CursorBitmap& bitmap) {
+    if (unlikely(IsSoftwareCursor())) {
+      m_sCursor.Width = 0;
+      m_sCursor.Height = 0;
+      m_sCursor.X = 0;
+      m_sCursor.Y = 0;
+    }
+
     CursorMask mask;
     std::memset(mask, ~0, sizeof(mask));
 
@@ -48,9 +77,40 @@ namespace dxvk {
 
     return D3D_OK;
   }
+
+
+  HRESULT D3D9Cursor::SetSoftwareCursor(UINT Width, UINT Height, UINT XHotSpot, UINT YHotSpot) {
+    // Make sure to hide the win32 cursor
+    ::SetCursor(nullptr);
+
+    if (unlikely(m_hCursor != nullptr)) {
+      ::DestroyCursor(m_hCursor);
+      m_hCursor = nullptr;
+    }
+
+    m_sCursor.Width  = Width;
+    m_sCursor.Height = Height;
+    m_sCursor.X      = XHotSpot;
+    m_sCursor.Y      = YHotSpot;
+
+    ShowCursor(m_visible);
+
+    return D3D_OK;
+  }
+
 #else
+  void D3D9Cursor::ResetCursor() {
+    Logger::warn("D3D9Cursor::ResetCursor: Not supported on current platform.");
+  }
+
+
   void D3D9Cursor::UpdateCursor(int X, int Y) {
     Logger::warn("D3D9Cursor::UpdateCursor: Not supported on current platform.");
+  }
+
+
+  void D3D9Cursor::RefreshSoftwareCursorPosition() {
+    Logger::warn("D3D9Cursor::RefreshSoftwareCursorPosition: Not supported on current platform.");
   }
 
 
@@ -62,6 +122,12 @@ namespace dxvk {
 
   HRESULT D3D9Cursor::SetHardwareCursor(UINT XHotSpot, UINT YHotSpot, const CursorBitmap& bitmap) {
     Logger::warn("D3D9Cursor::SetHardwareCursor: Not supported on current platform.");
+
+    return D3D_OK;
+  }
+
+  HRESULT D3D9Cursor::SetSoftwareCursor(UINT Width, UINT Height, UINT XHotSpot, UINT YHotSpot) {
+    Logger::warn("D3D9Cursor::SetSoftwareCursor: Not supported on current platform.");
 
     return D3D_OK;
   }
