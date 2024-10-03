b7a685eb17e69050ff5bfc6463ad723b71c338a9
WinterSnowfall <WinterSnowfall@users.noreply.github.com>
2024-10-04 00:27:45 +0300

    [d3d8] Capture VB stream state in D3D8 state blocks

--- src/d3d8/d3d8_state_block.h
+++ src/d3d8/d3d8_state_block.h
@@ -16,6 +16,7 @@ namespace dxvk {
     bool swvp     : 1;
 
     bit::bitset<d8caps::MAX_TEXTURE_STAGES> textures;
+    bit::bitset<d8caps::MAX_STREAMS>        streams;
 
     D3D8StateCapture()
       : vs(false)
@@ -24,6 +25,7 @@ namespace dxvk {
       , swvp(false) {
       // Ensure all bits are initialized to false
       textures.clearAll();
+      streams.clearAll();
     }
   };
 
@@ -54,9 +56,11 @@ namespace dxvk {
         m_capture.indices = true;
         m_capture.swvp    = true;
         m_capture.textures.setAll();
+        m_capture.streams.setAll();
       }
 
       m_textures.fill(nullptr);
+      m_streams.fill(D3D8VBOP());
     }
 
     ~D3D8StateBlock() {}
@@ -97,6 +101,13 @@ namespace dxvk {
       return D3D_OK;
     }
 
+    inline HRESULT SetStreamSource(UINT StreamNumber, IDirect3DVertexBuffer8* pStreamData, UINT Stride) {
+      m_streams[StreamNumber].buffer = pStreamData;
+      m_streams[StreamNumber].stride = Stride;
+      m_capture.streams.set(StreamNumber, true);
+      return D3D_OK;
+    }
+
     inline HRESULT SetIndices(IDirect3DIndexBuffer8* pIndexData, UINT BaseVertexIndex) {
       m_indices         = pIndexData;
       m_baseVertexIndex = BaseVertexIndex;
@@ -115,6 +126,11 @@ namespace dxvk {
     Com<d3d9::IDirect3DStateBlock9> m_stateBlock;
     D3DSTATEBLOCKTYPE               m_type;
 
+    struct D3D8VBOP {
+      IDirect3DVertexBuffer8*       buffer = nullptr;
+      UINT                          stride = 0;
+    };
+
   private: // State Data //
 
     D3D8StateCapture m_capture;
@@ -123,6 +139,7 @@ namespace dxvk {
     DWORD m_pixelShader;  // ps
 
     std::array<IDirect3DBaseTexture8*, d8caps::MAX_TEXTURE_STAGES>  m_textures; // textures
+    std::array<D3D8VBOP, d8caps::MAX_STREAMS>                       m_streams; // stream data
 
     IDirect3DIndexBuffer8*  m_indices = nullptr;  // indices
     UINT                    m_baseVertexIndex;    // indices
