bce4dbd8ccb295730dbde9d14d2a910cb38245c8
Blisto91 <Blisto@protonmail.com>
2024-10-06 20:45:11 +0200

    [d3d9] Silence unhandled render state D3DRS_ADAPTIVETESS_Y

--- src/d3d9/d3d9_device.cpp
+++ src/d3d9/d3d9_device.cpp
@@ -2463,6 +2463,9 @@ namespace dxvk {
           m_flags.set(D3D9DeviceFlag::DirtyFFVertexShader);
           break;
 
+        case D3DRS_ADAPTIVETESS_Y:
+          break;
+
         case D3DRS_ADAPTIVETESS_X:
         case D3DRS_ADAPTIVETESS_Z:
         case D3DRS_ADAPTIVETESS_W:
