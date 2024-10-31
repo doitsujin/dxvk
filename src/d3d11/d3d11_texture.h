7bc7b5636d98e26ec6639cfa77f85d1a1bbc26f4
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2024-10-31 22:05:12 +0100

    [d3d11] Tie mapped image memory type to map mode selection
    Allows us to keep video images in system memory in order to avoid
    wasting precious HVV.

--- src/d3d11/d3d11_texture.h
+++ src/d3d11/d3d11_texture.h
@@ -562,9 +562,7 @@ namespace dxvk {
             VkFormat              Format,
             VkFormatFeatureFlags2 Features) const;
     
-    VkMemoryPropertyFlags GetMemoryFlags() const;
-    
-    D3D11_COMMON_TEXTURE_MAP_MODE DetermineMapMode(
+    std::pair<D3D11_COMMON_TEXTURE_MAP_MODE, VkMemoryPropertyFlags> DetermineMapMode(
       const DxvkImageCreateInfo*  pImageInfo) const;
 
     D3D11_COMMON_TEXTURE_SUBRESOURCE_LAYOUT DetermineSubresourceLayout(
