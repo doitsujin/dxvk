51bffe6348713015eb793b7852f6ee3546464b24
Blisto91 <Blisto@protonmail.com>
2024-11-26 17:17:37 +0100

    [native] Include all supported wsi backend headers
    So applications actually can support multiple backends with the same .so

--- include/native/wsi/native_wsi.h
+++ include/native/wsi/native_wsi.h
@@ -2,12 +2,20 @@
 
 #ifdef DXVK_WSI_WIN32
 #error You shouldnt be using this code path.
-#elif DXVK_WSI_SDL3
+#endif
+
+#ifdef DXVK_WSI_SDL3
 #include "wsi/native_sdl3.h"
-#elif DXVK_WSI_SDL2
+#endif
+
+#ifdef DXVK_WSI_SDL2
 #include "wsi/native_sdl2.h"
-#elif DXVK_WSI_GLFW
+#endif
+
+#ifdef DXVK_WSI_GLFW
 #include "wsi/native_glfw.h"
-#else
+#endif
+
+#if !definedDXVK_WSI_SDL3) && !defined(DXVK_WSI_SDL2) && !defined(DXVK_WSI_GLFW)
 #error Unknown wsi!
 #endif
