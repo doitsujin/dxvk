0eeedf9259c5798f604cc51c58aa6f61966a96a0
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2025-01-13 11:31:35 +0100

    [hud] Allow returning HUD item objects to the caller

--- src/dxvk/hud/dxvk_hud_item.h
+++ src/dxvk/hud/dxvk_hud_item.h
@@ -91,7 +91,7 @@ namespace dxvk::hud {
      * \param [in] args Constructor arguments
      */
     template<typename T, typename... Args>
-    void add(const char* name, int32_t at, Args... args) {
+    Rc<T> add(const char* name, int32_t at, Args... args) {
       bool enable = m_enableFull;
 
       if (!enable) {
@@ -102,10 +102,14 @@ namespace dxvk::hud {
       if (at < 0 || at > int32_t(m_items.size()))
         at = m_items.size();
 
+      Rc<T> item;
+
       if (enable) {
-        m_items.insert(m_items.begin() + at,
-          new T(std::forward<Args>(args)...));
+        item = new T(std::forward<Args>(args)...);
+        m_items.insert(m_items.begin() + at, item);
       }
+
+      return item;
     }
 
     template<typename T>
