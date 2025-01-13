5119b3d6f62eb660a09d72e5929209e1ccec66b0
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2025-01-13 23:56:37 +0100

    [d3d9,d3d11] Remove numBackBuffers option
    Useless.

--- src/d3d9/d3d9_options.cpp
+++ src/d3d9/d3d9_options.cpp
@@ -49,7 +49,6 @@ namespace dxvk {
     this->strictConstantCopies          = config.getOption<bool>        ("d3d9.strictConstantCopies",          false);
     this->strictPow                     = config.getOption<bool>        ("d3d9.strictPow",                     true);
     this->lenientClear                  = config.getOption<bool>        ("d3d9.lenientClear",                  false);
-    this->numBackBuffers                = config.getOption<int32_t>     ("d3d9.numBackBuffers",                0);
     this->deferSurfaceCreation          = config.getOption<bool>        ("d3d9.deferSurfaceCreation",          false);
     this->samplerAnisotropy             = config.getOption<int32_t>     ("d3d9.samplerAnisotropy",             -1);
     this->maxAvailableMemory            = config.getOption<int32_t>     ("d3d9.maxAvailableMemory",            4096);
