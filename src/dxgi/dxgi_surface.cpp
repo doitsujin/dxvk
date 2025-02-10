f615594f22d40e58092d1d0e9056663b5811a66a
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2025-02-10 14:03:40 +0100

    [dxgi] Create dummy window for composition swap chains
    Obviously not a valid implementation, but allows composition swap chains
    to exist with some basic functionality.

--- src/dxgi/dxgi_surface.cpp
+++ src/dxgi/dxgi_surface.cpp
@@ -5,13 +5,15 @@
 namespace dxvk {
 
   DxgiSurfaceFactory::DxgiSurfaceFactory(PFN_vkGetInstanceProcAddr vulkanLoaderProc, HWND hWnd)
-  : m_vkGetInstanceProcAddr(vulkanLoaderProc), m_window(hWnd) {
-
+  : m_vkGetInstanceProcAddr(vulkanLoaderProc), m_window(hWnd), m_ownsWindow(!hWnd) {
+    if (!m_window)
+      m_window = CreateDummyWindow();
   }
 
 
   DxgiSurfaceFactory::~DxgiSurfaceFactory() {
-
+    if (m_ownsWindow)
+      DestroyDummyWindow();
   }
 
 
@@ -44,5 +46,47 @@ namespace dxvk {
           VkSurfaceKHR*             pSurface) {
     return wsi::createSurface(m_window, m_vkGetInstanceProcAddr, Instance, pSurface);
   }
-  
+
+
+  HWND DxgiSurfaceFactory::CreateDummyWindow() {
+#ifdef _WIN32
+    static std::atomic<bool> s_wndClassRegistered = { false };
+
+    HINSTANCE hInstance = ::GetModuleHandle(nullptr);
+
+    if (!s_wndClassRegistered.load(std::memory_order_acquire)) {
+      WNDCLASSEXW wndClass = { };
+      wndClass.cbSize = sizeof(wndClass);
+      wndClass.style = CS_HREDRAW | CS_VREDRAW;
+      wndClass.lpfnWndProc = &::DefWindowProcW;
+      wndClass.hInstance = hInstance;
+      wndClass.lpszClassName = L"DXVKDUMMYWNDCLASS";
+
+      ATOM atom = ::RegisterClassExW(&wndClass);
+
+      if (!atom)
+        Logger::warn("DxgiSurfaceFactory: Failed to register dummy window class");
+
+      s_wndClassRegistered.store(!!atom, std::memory_order_release);
+    }
+
+    HWND hWnd = ::CreateWindowW(L"DXVKDUMMYWNDCLASS", L"DXVKDUMMYWINDOW",
+      WS_OVERLAPPEDWINDOW, 0, 0, 320, 240, nullptr, nullptr, hInstance, nullptr);
+
+    if (!hWnd)
+      Logger::err("DxgiSurfaceFactory: Failed to create dummy window");
+
+    return hWnd;
+#else
+    return nullptr;
+#endif
+  }
+
+
+  void DxgiSurfaceFactory::DestroyDummyWindow() {
+#ifdef _WIN32
+    DestroyWindow(m_window);
+#endif
+  }
+
 }
