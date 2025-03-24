e76e6db1c1d187f1b711661906734954c84f4996
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2025-03-24 01:08:27 +0100

    [dxbc] Don't export point size by default

--- src/dxbc/dxbc_options.cpp
+++ src/dxbc/dxbc_options.cpp
@@ -41,13 +41,8 @@ namespace dxvk {
     enableSampleShadingInterlock = device->features().extFragmentShaderInterlock.fragmentShaderSampleInterlock;
     supportsTightIcbPacking  = device->features().vk12.uniformBufferStandardLayout;
 
-    // Qcom just breaks for no reason if we export point size,
-    // even in an environment where doing so is required.
-    needsPointSizeExport = !device->features().khrMaintenance5.maintenance5
-                        && !device->adapter()->matchesDriver(VK_DRIVER_ID_QUALCOMM_PROPRIETARY);
-
     // ANV up to mesa 25.0.2 breaks when we *don't* explicitly write point size
-    needsPointSizeExport |= device->adapter()->matchesDriver(VK_DRIVER_ID_INTEL_OPEN_SOURCE_MESA, Version(), Version(25, 0, 3));
+    needsPointSizeExport = device->adapter()->matchesDriver(VK_DRIVER_ID_INTEL_OPEN_SOURCE_MESA, Version(), Version(25, 0, 3));
 
     // Figure out float control flags to match D3D11 rules
     if (options.floatControls) {
