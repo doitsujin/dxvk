a80c674078b5f77716715da7286b9a55c23f2434
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2025-03-31 12:00:18 +1000

    [util] Fix BSR undefiled behavior when input is 0
    Test on native LZCNT it should return 64/32, so implement for this BSR fallback

--- src/util/util_bit.h
+++ src/util/util_bit.h
@@ -156,7 +156,13 @@ namespace dxvk::bit {
   }
 
   inline uint32_t lzcnt(uint32_t n) {
-    #if (defined(_MSC_VER) && !defined(__clang__)) || defined(__LZCNT__)
+    #if defined(_MSC_VER) && !defined(__AVX2__)
+    unsigned long bsr;
+    if(n == 0)
+      return 32;
+    _BitScanReverse(&bsr, n);
+    return 31-bsr;
+    #elif (defined(_MSC_VER) && !defined(__clang__)) || defined(__LZCNT__)
     return _lzcnt_u32(n);
     #elif defined(__GNUC__) || defined(__clang__)
     return n != 0 ? __builtin_clz(n) : 32;
@@ -176,16 +182,11 @@ namespace dxvk::bit {
   }
 
   inline uint32_t lzcnt(uint64_t n) {
-    #if defined(_MSC_VER) && !defined(__AVX2__)
+    #if defined(_MSC_VER) && !defined(__AVX2__) && defined(DXVK_ARCH_X86_64)
     unsigned long bsr;
-    #if defined(DXVK_ARCH_X86_64)
+    if(n == 0)
+      return 64;
     _BitScanReverse64(&bsr, n);
-    #else
-    if  (_BitScanReverse(&bsr, n >> 32))
-        bsr += 32;
-    else
-        _BitScanReverse(&bsr, n & 0xffffffff);
-    #endif
     return 63-bsr;
     #elif defined(DXVK_ARCH_X86_64) && ((defined(_MSC_VER) && !defined(__clang__)) || defined(__LZCNT__))
     return _lzcnt_u64(n);
