6cee1c1634fa4b0ae9a2c356d889d66bfdf2c531
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2025-03-31 16:21:29 +0200

    [d3d11] Add feature level 12_2 enum for logging purposes
    12_2 is not a valid feature level for D3D11, but some apps will try
    regardless.

--- src/d3d11/d3d11_enums.cpp
+++ src/d3d11/d3d11_enums.cpp
@@ -11,6 +11,7 @@ std::ostream& operator << (std::ostream& os, D3D_FEATURE_LEVEL e) {
     ENUM_NAME(D3D_FEATURE_LEVEL_11_1);
     ENUM_NAME(D3D_FEATURE_LEVEL_12_0);
     ENUM_NAME(D3D_FEATURE_LEVEL_12_1);
+    ENUM_NAME(D3D_FEATURE_LEVEL_12_2);
     ENUM_DEFAULT(e);
   }
 }
