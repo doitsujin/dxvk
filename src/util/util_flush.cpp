5d0923195cb63f80c7ce8fd19421b5e268145102
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2025-04-10 18:48:38 +0200

    [util] Remove medium hint for submission heuristic
    Unused.

--- src/util/util_flush.cpp
+++ src/util/util_flush.cpp
@@ -45,7 +45,6 @@ namespace dxvk {
         return chunkCount >= minChunkCount;
       }
 
-      case GpuFlushType::ImplicitMediumHint:
       case GpuFlushType::ImplicitWeakHint: {
         // Aim for a higher number of chunks per submission with
         // a weak hint in order to avoid submitting too often.
