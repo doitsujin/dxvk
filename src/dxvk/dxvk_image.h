c5e862ce7094eba65edb523ac88b58b3d7291d5f
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2025-05-19 13:36:03 +0200

    [dxvk] Add image layout property to image view key

--- src/dxvk/dxvk_image.h
+++ src/dxvk/dxvk_image.h
@@ -139,6 +139,28 @@ namespace dxvk {
     void decRef();
 
     /**
+     * \brief Image view descriptor for the default type
+     *
+     * The default view type is guaranteed to be
+     * supported by the image view, and should be
+     * preferred over picking a different type.
+     * \returns Image view handle
+     */
+    const DxvkDescriptor* getDescriptor() {
+      return getDescriptor(m_key.viewType);
+    }
+
+    /**
+     * \brief Image view handle for a given view type
+     *
+     * If the view does not support the requested image
+     * view type, \c VK_NULL_HANDLE will be returned.
+     * \param [in] viewType The requested view type
+     * \returns The image view handle
+     */
+    const DxvkDescriptor* getDescriptor(VkImageViewType viewType);
+
+    /**
      * \brief Image view handle for the default type
      *
      * The default view type is guaranteed to be
@@ -158,7 +180,10 @@ namespace dxvk {
      * \param [in] viewType The requested view type
      * \returns The image view handle
      */
-    VkImageView handle(VkImageViewType viewType);
+    VkImageView handle(VkImageViewType viewType) {
+      auto descriptor = getDescriptor(viewType);
+      return likely(descriptor) ? descriptor->legacy.image.imageView : VK_NULL_HANDLE;
+    }
 
     /**
      * \brief Image view type
@@ -234,27 +259,6 @@ namespace dxvk {
     VkImageSubresourceRange imageSubresources() const;
 
     /**
-     * \brief Picks an image layout
-     * \see DxvkImage::pickLayout
-     */
-    VkImageLayout pickLayout(VkImageLayout layout) const;
-
-    /**
-     * \brief Retrieves descriptor info
-     * 
-     * \param [in] type Exact view type
-     * \param [in] layout Image layout
-     * \returns Image descriptor
-     */
-    DxvkLegacyDescriptor getDescriptor(VkImageViewType type, VkImageLayout layout) {
-      DxvkLegacyDescriptor result = { };
-      result.image.sampler = VK_NULL_HANDLE;
-      result.image.imageView = handle(type);
-      result.image.imageLayout = layout;
-      return result;
-    }
-
-    /**
      * \brief Checks whether this view matches another
      *
      * \param [in] view The other view to check
@@ -286,6 +290,15 @@ namespace dxvk {
         this->imageSubresources(),
         view->imageSubresources());
     }
+
+    /**
+     * \brief Queries the view layout
+     *
+     * If no layout was explicitly specified for the view, this
+     * will return a suitable layout for the given usage.
+     * \returns Image view layout
+     */
+    VkImageLayout getLayout() const;
 
     /**
      * \brief Queries the default image layout
@@ -788,25 +801,21 @@ namespace dxvk {
   }
 
 
-  inline VkImageLayout DxvkImageView::pickLayout(VkImageLayout layout) const {
-    return m_image->pickLayout(layout);
-  }
-
-
-  inline VkImageView DxvkImageView::handle(VkImageViewType viewType) {
+  inline const DxvkDescriptor* DxvkImageView::getDescriptor(VkImageViewType viewType) {
     viewType = viewType != VK_IMAGE_VIEW_TYPE_MAX_ENUM ? viewType : m_key.viewType;
 
     if (unlikely(m_version < m_image->m_version))
       updateViews();
 
-    if (unlikely(!m_views[viewType])) {
+    if (unlikely(!m_views[viewType]))
       m_views[viewType] = createView(viewType);
 
-      if (!m_views[viewType])
-        return VK_NULL_HANDLE;
-    }
+    return m_views[viewType];
+  }
 
-    return m_views[viewType]->legacy.image.imageView;
+
+  inline VkImageLayout DxvkImageView::getLayout() const {
+    return m_image->pickLayout(m_key.layout);
   }
 
 
