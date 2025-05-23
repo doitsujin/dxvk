9b7e78c35c69bd94aa6625b698d3a5d06ed37bb2
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2025-05-23 15:39:57 +0200

    [dxvk] Enable descriptor indexing features

--- src/dxvk/dxvk_adapter.cpp
+++ src/dxvk/dxvk_adapter.cpp
@@ -357,6 +357,14 @@ namespace dxvk {
     // Required internally
     enabledFeatures.vk12.bufferDeviceAddress = VK_TRUE;
 
+    // Features required for bindless samplers
+    enabledFeatures.core.features.shaderSampledImageArrayDynamicIndexing = VK_TRUE;
+    enabledFeatures.vk12.descriptorIndexing = VK_TRUE;
+    enabledFeatures.vk12.descriptorBindingSampledImageUpdateAfterBind = VK_TRUE;
+    enabledFeatures.vk12.descriptorBindingUpdateUnusedWhilePending = VK_TRUE;
+    enabledFeatures.vk12.descriptorBindingPartiallyBound = VK_TRUE;
+    enabledFeatures.vk12.runtimeDescriptorArray = VK_TRUE;
+
     // Only enable the base image robustness feature if robustness 2 isn't
     // supported, since this is only a subset of what we actually want.
     enabledFeatures.vk13.robustImageAccess =
