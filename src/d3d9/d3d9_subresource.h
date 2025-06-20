d92325a083a002b8ed9e77061d931b1965506457
Robin Kertels <robin.kertels@gmail.com>
2025-06-20 20:30:23 +0200

    [d3d9] Remove redundant GetImageView
    Only used for presentation and we can just use
    GetSampleView there.

--- src/d3d9/d3d9_subresource.h
+++ src/d3d9/d3d9_subresource.h
@@ -76,18 +76,6 @@ namespace dxvk {
       return m_texture->CalcSubresource(m_face, m_mipLevel);
     }
 
-    inline const Rc<DxvkImageView>& GetImageView(bool Srgb) {
-      Rc<DxvkImageView>& view = m_sampleView.Pick(Srgb);
-
-      if (unlikely(!view && !IsNull())) {
-        view = m_texture->CreateView(m_face, m_mipLevel,
-          VK_IMAGE_USAGE_SAMPLED_BIT, VK_IMAGE_LAYOUT_UNDEFINED,
-          Srgb && m_isSrgbCompatible);
-      }
-
-      return view;
-    }
-
     inline const Rc<DxvkImageView>& GetRenderTargetView(bool Srgb) {
       Rc<DxvkImageView>& view = m_renderTargetView.Pick(Srgb);
 
@@ -138,7 +126,6 @@ namespace dxvk {
       // Only used for swap chain back buffers that don't
       // have a container and all have identical properties
       std::swap(m_texture,          Other->m_texture);
-      std::swap(m_sampleView,       Other->m_sampleView);
       std::swap(m_renderTargetView, Other->m_renderTargetView);
     }
 
@@ -153,8 +140,7 @@ namespace dxvk {
     UINT                    m_mipLevel         : 16;
     UINT                    m_isSrgbCompatible : 1;
     UINT                    m_isNull           : 1;
-  
-    D3D9ColorView           m_sampleView;
+
     D3D9ColorView           m_renderTargetView;
 
     Rc<DxvkImageView>       m_dsvReadWrite;
