ef6c8f060492095582e4c9b882308e9b9c8f27f4
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2025-08-27 17:22:43 +0200

    [d3d11] Remove D3D11VkExtShader interface

--- src/d3d11/d3d11_interfaces.h
+++ src/d3d11/d3d11_interfaces.h
@@ -31,26 +31,6 @@ enum D3D11_VK_BARRIER_CONTROL : uint32_t {
 
 
 /**
- * \brief Extended shader interface
- */
-MIDL_INTERFACE("bb8a4fb9-3935-4762-b44b-35189a26414a")
-ID3D11VkExtShader : public IUnknown {
-  /**
-   * \brief Retrieves SPIR-V code from a shader object
-   *
-   * \param [in,out] pCodeSize Shader code size, in bytes. If
-   *    \ref pCode is \c nullptr, this will return the total
-   *    code size, otherwise the number of bytes written.
-   * \param [out] pCode SPIR-V shader code
-   * \returns \c S_OK, or \c S_FALSE if the buffer was too small
-   */
-  virtual HRESULT STDMETHODCALLTYPE GetSpirvCode(
-          SIZE_T*                 pCodeSize,
-          void*                   pCode) = 0;
-};
-
-
-/**
  * \brief Extended D3D11 device
  * 
  * Introduces a method to check for extension support.
@@ -245,7 +225,6 @@ ID3DLowLatencyDevice : public IUnknown {
 
 
 #ifndef _MSC_VER
-__CRT_UUID_DECL(ID3D11VkExtShader,         0xbb8a4fb9,0x3935,0x4762,0xb4,0x4b,0x35,0x18,0x9a,0x26,0x41,0x4a);
 __CRT_UUID_DECL(ID3D11VkExtDevice,         0x8a6e3c42,0xf74c,0x45b7,0x82,0x65,0xa2,0x31,0xb6,0x77,0xca,0x17);
 __CRT_UUID_DECL(ID3D11VkExtDevice1,        0xcfcf64ef,0x9586,0x46d0,0xbc,0xa4,0x97,0xcf,0x2c,0xa6,0x1b,0x06);
 __CRT_UUID_DECL(ID3D11VkExtContext,        0xfd0bca13,0x5cb6,0x4c3a,0x98,0x7e,0x47,0x50,0xde,0x2c,0xa7,0x91);
