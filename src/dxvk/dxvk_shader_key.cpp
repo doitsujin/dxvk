7dedf92b72455cfed4cec02dcd7ece7739ade8af
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2025-08-31 15:50:26 +0200

    [dxvk] Add new shader look-up key class

--- src/dxvk/dxvk_shader_key.cpp
+++ src/dxvk/dxvk_shader_key.cpp
@@ -2,6 +2,115 @@
 
 namespace dxvk {
 
+  DxvkShaderHash::DxvkShaderHash() {
+
+  }
+
+
+  DxvkShaderHash::DxvkShaderHash(
+          VkShaderStageFlagBits stage,
+          uint32_t              codeSize,
+    const uint8_t*              hash,
+          size_t                hashSize)
+  : DxvkShaderHash(stage, codeSize, hash, hashSize, nullptr, 0u) { }
+
+
+  DxvkShaderHash::DxvkShaderHash(
+          VkShaderStageFlagBits stage,
+          uint32_t              codeSize,
+    const uint8_t*              hash,
+          size_t                hashSize,
+    const uint8_t*              metaHash,
+          size_t                metaSize)
+  : m_stage(uint16_t(stage)), m_xfb(metaSize ? 1u : 0u), m_size(codeSize) {
+    size_t index = 0u;
+
+    for (size_t i = 0u; i < hashSize; i += 4u) {
+      m_hash[index] ^= getDword(&hash[i]);
+      index = (index + 1u) % m_hash.size();
+    }
+
+    for (size_t i = 0u; i < metaSize; i += 4u) {
+      m_hash[index] ^= getDword(&metaHash[i]);
+      index = (index + 1u) % m_hash.size();
+    }
+  }
+
+
+  std::string DxvkShaderHash::toString() const {
+    std::string name;
+    name.reserve(48u);
+
+    if (m_xfb) {
+      name = "xfb";
+    } else {
+      switch (m_stage) {
+        case VK_SHADER_STAGE_VERTEX_BIT: name = "vs"; break;
+        case VK_SHADER_STAGE_TESSELLATION_CONTROL_BIT: name = "tcs"; break;
+        case VK_SHADER_STAGE_TESSELLATION_EVALUATION_BIT: name = "tes"; break;
+        case VK_SHADER_STAGE_GEOMETRY_BIT: name = "gs"; break;
+        case VK_SHADER_STAGE_FRAGMENT_BIT: name = "fs"; break;
+        case VK_SHADER_STAGE_COMPUTE_BIT: name = "cs"; break;
+        default: name = "shdr";
+      }
+    }
+
+    name += ".";
+
+    for (size_t i = 0u; i < m_hash.size(); i++) {
+      for (uint32_t j = 0u; j < 4u; j++) {
+        name += toHex(m_hash[i] >> (8u * j + 4u));
+        name += toHex(m_hash[i] >> (8u * j));
+      }
+    }
+
+    return name;
+  }
+
+
+  bool DxvkShaderHash::eq(const DxvkShaderHash& other) const {
+    bool eq = m_stage == other.m_stage
+           && m_xfb == other.m_xfb
+           && m_size == other.m_size;
+
+    for (size_t i = 0u; i < m_hash.size(); i++)
+      eq = eq && m_hash[i] == other.m_hash[i];
+
+    return eq;
+  }
+
+
+  size_t DxvkShaderHash::hash() const {
+    DxvkHashState hash = { };
+    hash.add(m_stage);
+    hash.add(m_xfb);
+    hash.add(m_size);
+
+    for (auto dw : m_hash)
+      hash.add(dw);
+
+    return hash;
+  }
+
+
+  size_t DxvkShaderHash::getDword(const uint8_t* dw) {
+    return  (uint32_t(dw[0u]))
+          | (uint32_t(dw[1u]) << 8u)
+          | (uint32_t(dw[2u]) << 16u)
+          | (uint32_t(dw[3u]) << 24u);
+  }
+
+
+  char DxvkShaderHash::toHex(uint8_t nibble) {
+    static const std::array<char, 16u> ch = {
+      '0', '1', '2', '3', '4', '5', '6', '7',
+      '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
+    };
+
+    return ch[nibble & 0xfu];
+  }
+
+
   DxvkShaderKey::DxvkShaderKey()
   : m_type(0),
     m_sha1(Sha1Hash::compute(nullptr, 0)) { }
--- src/dxvk/dxvk_shader_key.h
+++ src/dxvk/dxvk_shader_key.h
@@ -6,6 +6,100 @@
 namespace dxvk {
 
   /**
+   * \brief Shader look-up key
+   *
+   * Stores the shader hash itself, as well as some basic
+   * metadata like the shader type or hashed xfb metadata.
+   */
+  class DxvkShaderHash {
+
+  public:
+
+    DxvkShaderHash();
+
+    /**
+     * \brief Initializes shader hash
+     *
+     * \param [in] stage Shader stage
+     * \param [in] codeSize Shader code size, in bytes
+     * \param [in] hash Pointer to shader hash
+     * \param [in] hashSize Shader hash size, in bytes
+     */
+    DxvkShaderHash(
+            VkShaderStageFlagBits stage,
+            uint32_t              codeSize,
+      const uint8_t*              hash,
+            size_t                hashSize);
+
+    /**
+     * \brief Initializes shader hash
+     *
+     * \param [in] stage Shader stage
+     * \param [in] codeSize Shader code size, in bytes
+     * \param [in] hash Pointer to shader hash
+     * \param [in] hashSize Shader hash size, in bytes
+     * \param [in] metaHash Metadata hash
+     * \param [in] metaSize Metadata hash size, in bytes
+     */
+    DxvkShaderHash(
+            VkShaderStageFlagBits stage,
+            uint32_t              codeSize,
+      const uint8_t*              hash,
+            size_t                hashSize,
+      const uint8_t*              metaHash,
+            size_t                metaSize);
+
+    /**
+     * \brief Shader stage
+     * \returns Shader stage
+     */
+    VkShaderStageFlagBits stage() const {
+      return VkShaderStageFlagBits(m_stage);
+    }
+
+    /**
+     * \brief Whether shader was created using streamout metadata
+     * \returns \c true if the shader has transform feedback metadata
+     */
+    bool hasXfb() const {
+      return m_xfb;
+    }
+
+    /**
+     * \brief Generates shader name for the given hash
+     * \returns Shader name to use for tooling etc.
+     */
+    std::string toString() const;
+
+    /**
+     * \brief Compares two shader hashes
+     *
+     * \param [in] other Other hash to compare to
+     * \returns \c true if the hashes are equal
+     */
+    bool eq(const DxvkShaderHash& other) const;
+
+    /**
+     * \brief Computes look-up hash for shader
+     * \returns Hash for internal hash tables
+     */
+    size_t hash() const;
+
+  private:
+
+    uint16_t                  m_stage = -1;
+    uint16_t                  m_xfb   = 0u;
+    uint32_t                  m_size  = 0u;
+    std::array<uint32_t, 4u>  m_hash  = { };
+
+    static size_t getDword(const uint8_t* dw);
+
+    static char toHex(uint8_t nibble);
+
+  };
+
+
+  /**
    * \brief Shader key
    * 
    * Provides a unique key that can be used
