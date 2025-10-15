0ad714f7d46cba6c3724e083375250b37608581f
Rémi Bernon <rbernon@codeweavers.com>
2025-10-15 13:26:45 +0200

    [util] Import D3DKMT functions directly
    And provide dummy fallbacks for non-Win32 platforms.
    
    This also updates the definitions to better match the Windows SDK
    headers, making future additions easier.

--- src/util/util_gdi.cpp
+++ src/util/util_gdi.cpp
@@ -3,40 +3,13 @@
 
 namespace dxvk {
 
-#ifdef _WIN32
-  static HMODULE GetGDIModule() {
-    static HMODULE module = LoadLibraryA("gdi32.dll");
-    return module;
-  }
-
-  NTSTATUS D3DKMTCreateDCFromMemory(D3DKMT_CREATEDCFROMMEMORY* Arg1) {
-    static auto func = (D3DKMTCreateDCFromMemoryType)
-      GetProcAddress(GetGDIModule(), "D3DKMTCreateDCFromMemory");
-
-    if (func != nullptr)
-      return func(Arg1);
-
-    Logger::warn("D3DKMTCreateDCFromMemory: Unable to query proc address.");
-    return -1;
-  }
-
-  NTSTATUS D3DKMTDestroyDCFromMemory(D3DKMT_DESTROYDCFROMMEMORY* Arg1) {
-    static auto func = (D3DKMTDestroyDCFromMemoryType)
-      GetProcAddress(GetGDIModule(), "D3DKMTDestroyDCFromMemory");
-
-    if (func != nullptr)
-      return func(Arg1);
-
-    Logger::warn("D3DKMTDestroyDCFromMemory: Unable to query proc address.");
-    return -1;
-  }
-#else
-  NTSTATUS D3DKMTCreateDCFromMemory(D3DKMT_CREATEDCFROMMEMORY* Arg1) {
+#ifndef _WIN32
+  NTSTATUS D3DKMTCreateDCFromMemory(D3DKMT_CREATEDCFROMMEMORY *desc) {
     Logger::warn("D3DKMTCreateDCFromMemory: Not available on this platform.");
     return -1;
   }
 
-  NTSTATUS D3DKMTDestroyDCFromMemory(D3DKMT_DESTROYDCFROMMEMORY* Arg1) {
+  NTSTATUS D3DKMTDestroyDCFromMemory(const D3DKMT_DESTROYDCFROMMEMORY *desc) {
     Logger::warn("D3DKMTDestroyDCFromMemory: Not available on this platform.");
     return -1;
   }
