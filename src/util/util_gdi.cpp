3fac2fae19d6b83fcf2d91f353afc3d2d0db8b14
Rémi Bernon <rbernon@codeweavers.com>
2025-10-15 13:49:21 +0200

    [dxvk] Open adapter D3DKMT handle from the device LUID

--- src/util/util_gdi.cpp
+++ src/util/util_gdi.cpp
@@ -4,6 +4,11 @@
 namespace dxvk {
 
 #ifndef _WIN32
+  NTSTATUS D3DKMTCloseAdapter(const D3DKMT_CLOSEADAPTER *desc) {
+    Logger::warn("D3DKMTCloseAdapter: Not available on this platform.");
+    return -1;
+  }
+
   NTSTATUS D3DKMTCreateDCFromMemory(D3DKMT_CREATEDCFROMMEMORY *desc) {
     Logger::warn("D3DKMTCreateDCFromMemory: Not available on this platform.");
     return -1;
@@ -11,6 +16,11 @@ namespace dxvk {
 
   NTSTATUS D3DKMTDestroyDCFromMemory(const D3DKMT_DESTROYDCFROMMEMORY *desc) {
     Logger::warn("D3DKMTDestroyDCFromMemory: Not available on this platform.");
+    return -1;
+  }
+
+  NTSTATUS D3DKMTOpenAdapterFromLuid(D3DKMT_OPENADAPTERFROMLUID *desc) {
+    Logger::warn("D3DKMTOpenAdapterFromLuid: Not available on this platform.");
     return -1;
   }
 #endif
