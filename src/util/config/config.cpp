a0358f8589b03acdc955b856d8959fd4c6b60a3a
Robin Kertels <robin.kertels@gmail.com>
2025-12-04 17:48:31 +0100

    [util] Remove unnecessary configs

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -1132,18 +1132,6 @@ namespace dxvk {
       { "d3d9.memoryTrackTest",             "True" },
       { "d3d9.maxAvailableMemory",          "1024" },
     }} },
-    /* Red Faction                                *
-     * Fixes crashing when starting a new game    */
-    { R"(\\RF\.exe$)", {{
-      { "d3d9.allowDirectBufferMapping",   "False" },
-    }} },
-    /* Commandos 3                                *
-     * The game doesn't use NOOVERWRITE properly  *
-     * and reads from actively modified buffers,  *
-     * which causes graphical glitches at times   */
-    { R"(\\Commandos3\.exe$)", {{
-      { "d3d9.allowDirectBufferMapping",   "False" },
-    }} },
     /* Motor City Online                          */
     { R"(\\MCity_d\.exe$)", {{
       { "d3d9.cachedWriteOnlyBuffers",      "True" },
