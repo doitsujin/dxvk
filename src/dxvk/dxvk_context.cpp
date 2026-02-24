59eb3086c0f61884a5c7fc2c6c53ec5c3cc94e2c
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2026-02-24 22:30:40 +0100

    [dxvk] Re-enable unsynchronized render passes

--- src/dxvk/dxvk_context.cpp
+++ src/dxvk/dxvk_context.cpp
@@ -5726,9 +5726,6 @@ namespace dxvk {
 
 
   bool DxvkContext::renderPassStartUnsynchronized() {
-    // TODO re-enable once we iron out the bugs.
-    return false;
-
     // Don't even try if there is a depth buffer bound, this is most likely
     // either a shadow pass or an otherwise expensive main render pass.
     //
