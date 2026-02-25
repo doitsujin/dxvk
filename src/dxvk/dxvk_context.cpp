67a1e562f1e0cb6f09594426cf53b85c001f78ee
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2026-02-25 03:54:39 +0100

    [dxvk] Fix maintenance9 barrier check for resource relocation
    Checking the flag doesn't work here because the flag can change
    between barriers. Always transitioning all 3D slices is safe
    here though.

--- src/dxvk/dxvk_context.cpp
+++ src/dxvk/dxvk_context.cpp
@@ -8186,7 +8186,7 @@ namespace dxvk {
             dstBarrier.image = info.storage->getImageInfo().image;
             dstBarrier.subresourceRange = subresourceRange;
 
-            if (info.image->info().flags & VK_IMAGE_CREATE_2D_ARRAY_COMPATIBLE_BIT) {
+            if (info.image->info().type == VK_IMAGE_TYPE_3D) {
               dstBarrier.subresourceRange.baseArrayLayer = 0u;
               dstBarrier.subresourceRange.layerCount = VK_REMAINING_ARRAY_LAYERS;
             }
@@ -8205,7 +8205,7 @@ namespace dxvk {
             srcBarrier.image = oldStorage->getImageInfo().image;
             srcBarrier.subresourceRange = subresourceRange;
 
-            if (info.image->info().flags & VK_IMAGE_CREATE_2D_ARRAY_COMPATIBLE_BIT) {
+            if (info.image->info().type == VK_IMAGE_TYPE_3D) {
               srcBarrier.subresourceRange.baseArrayLayer = 0u;
               srcBarrier.subresourceRange.layerCount = VK_REMAINING_ARRAY_LAYERS;
             }
@@ -8338,7 +8338,7 @@ namespace dxvk {
               dstBarrier.image = info.storage->getImageInfo().image;
               dstBarrier.subresourceRange = vk::makeSubresourceRange(region.dstSubresource);
 
-              if (info.image->info().flags & VK_IMAGE_CREATE_2D_ARRAY_COMPATIBLE_BIT) {
+              if (info.image->info().type == VK_IMAGE_TYPE_3D) {
                 dstBarrier.subresourceRange.baseArrayLayer = 0u;
                 dstBarrier.subresourceRange.layerCount = VK_REMAINING_ARRAY_LAYERS;
               }
