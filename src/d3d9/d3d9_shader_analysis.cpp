6dbf9ac97548e913603481ead47458cdb63e9bd3
Robin Kertels <robin.kertels@gmail.com>
2026-05-19 19:46:12 +0200

    [d3d9] Write new shader analysis prepass
    The current compiler collects a bunch of data that we'll
    still need with the new compiler. (Like for example the length
    of the shader bytecode)
    This analysis prepass will collect that data in the future.

---
+++ src/d3d9/d3d9_shader_analysis.cpp
@@ -0,0 +1,301 @@
+#include "../dxvk/dxvk_include.h"
+
+#include "d3d9_shader_analysis.h"
+#include "d3d9_util.h"
+
+#include <utility>
+
+#include "../util/log/log.h"
+
+using namespace dxbc_spv::sm3;
+
+namespace dxvk {
+
+  D3D9ShaderAnalysis::D3D9ShaderAnalysis(dxbc_spv::util::ByteReader code, bool isSWVP)
+    : m_isSWVP(isSWVP) {
+    if (!code) {
+      Logger::err("No code chunk found in shader.");
+      return;
+    }
+
+    if (Parser parser = Parser(code)) {
+      m_length = 0u;
+      RunAnalysis(parser);
+    } else {
+      Logger::err("Failed to parse code chunk.");
+    }
+  }
+
+  D3D9ShaderAnalysis::D3D9ShaderAnalysis(D3D9ShaderAnalysis&& other)
+    : m_isSWVP(other.m_isSWVP), m_length(other.m_length), m_shaderInfo(other.m_shaderInfo),
+      m_constants(other.m_constants), m_immediateConstants(std::move(other.m_immediateConstants)),
+      m_usedRTs(other.m_usedRTs), m_usedSamplers(other.m_usedSamplers), m_imageViewTypes(other.m_imageViewTypes),
+      m_inputSignature(std::move(other.m_inputSignature)) {
+    other.m_length = 0u;
+  }
+
+  D3D9ShaderAnalysis::D3D9ShaderAnalysis(const D3D9ShaderAnalysis& other)
+    : m_isSWVP(other.m_isSWVP), m_length(other.m_length), m_shaderInfo(other.m_shaderInfo),
+      m_constants(other.m_constants), m_immediateConstants(other.m_immediateConstants), m_usedRTs(other.m_usedRTs),
+      m_usedSamplers(other.m_usedSamplers), m_imageViewTypes(other.m_imageViewTypes),
+      m_inputSignature(other.m_inputSignature) { }
+
+  bool D3D9ShaderAnalysis::RunAnalysis(Parser& parser) {
+    m_shaderInfo = parser.getShaderInfo();
+
+    if (m_shaderInfo.getVersion().first == 1u && m_shaderInfo.getType() == dxbc_spv::sm3::ShaderType::ePixel)
+      m_usedRTs = 0b1u;
+
+    while (parser) {
+      dxbc_spv::sm3::Instruction instruction = parser.parseInstruction();
+
+      if (!HandleInstruction(instruction))
+        return false;
+    }
+
+    m_length = parser.getByteOffset();
+
+    // Shift up these sampler bits so we can just
+    // do an or per-draw in the device.
+    // We shift by 17 because 16 ps samplers + 1 dmap (tess)
+    if (m_shaderInfo.getType() == ShaderType::eVertex)
+      m_usedSamplers <<= FirstVSSamplerSlot;
+
+    return true;
+  }
+
+  bool D3D9ShaderAnalysis::HandleInstruction(const Instruction& op) {
+    auto matrixSize = getMatrixSize(op.getOpCode());
+
+    /* Determine whether we're accessing float constants dynamically
+     * because in that case, we'll need to copy the immediate constants
+     * into the constant buffer inside DXVK. */
+    for (uint32_t i = 0u; i < op.getSrcCount(); i++) {
+      const auto& src = op.getSrc(i);
+      auto registerType = src.getRegisterType();
+
+      uint32_t index = src.getIndex();
+
+      if (i && matrixSize)
+        index += matrixSize->second - 1u;
+
+      switch (registerType) {
+        case RegisterType::eConstInt:
+          m_constants.intCount = std::max(m_constants.intCount, index + 1u);
+          break;
+
+        case RegisterType::eConstBool: {
+          m_constants.boolCount = std::max(m_constants.boolCount, index + 1u);
+
+          if (!m_isSWVP) // SWVP raises the constant limit too high to use bit mask.
+            m_constants.boolMask |= 1u << index;
+        } break;
+
+        case RegisterType::eConst:
+        case RegisterType::eConst2:
+        case RegisterType::eConst3:
+        case RegisterType::eConst4: {
+          m_constants.floatCount = std::max(m_constants.floatCount, index + 1u);
+
+          if (src.hasRelativeAddressing()) {
+            uint32_t hwvpFloatConstantsCount = GetShaderInfo().getType() == ShaderType::ePixel ? MaxFloatConstantsPS : MaxFloatConstantsVS;
+
+            m_constants.floatCount = std::max(m_constants.floatCount,
+              m_isSWVP ? MaxFloatConstantsSoftware : hwvpFloatConstantsCount);
+
+            m_constants.floatsAccessedDynamically = true;
+          }
+        } break;
+
+        default: break;
+      }
+    }
+
+    if (GetShaderInfo().getType() == ShaderType::ePixel
+      && op.hasDst()
+      && op.getDst().getRegisterType() == RegisterType::eColorOut) {
+      m_usedRTs |= 1u << op.getDst().getIndex();
+    }
+
+    switch (op.getOpCode()) {
+      case OpCode::eDef:
+      case OpCode::eDefI:
+      case OpCode::eDefB:
+        if (!HandleDef(op))
+          return false;
+        break;
+
+      case OpCode::eTexLd:
+      case OpCode::eTexBem:
+      case OpCode::eTexBemL:
+      case OpCode::eTexReg2Ar:
+      case OpCode::eTexReg2Gb:
+      case OpCode::eTexM3x2Tex:
+      case OpCode::eTexM3x3Tex:
+      case OpCode::eTexM3x3Spec:
+      case OpCode::eTexM3x3VSpec:
+      case OpCode::eTexReg2Rgb:
+      case OpCode::eTexDp3Tex:
+      case OpCode::eTexM3x2Depth:
+      case OpCode::eTexDp3:
+      case OpCode::eTexM3x3:
+      case OpCode::eTexLdd:
+      case OpCode::eTexLdl:
+        if (!HandleTextureSample(op))
+          return false;
+        break;
+
+      case OpCode::eDcl:
+        if (!HandleDcl(op))
+          return false;
+        break;
+
+      default: break;
+    }
+
+    return true;
+  }
+
+  bool D3D9ShaderAnalysis::HandleDef(const Instruction& op) {
+    dxbc_spv_assert(op.hasDst());
+    uint32_t index = op.getDst().getIndex();
+
+    if (op.getOpCode() == OpCode::eDef) {
+      m_immediateConstants.floatCount = std::max(m_immediateConstants.floatCount, index + 1u);
+
+      dxbc_spv_assert(op.hasImm());
+      auto imm = op.getImm();
+
+      Vector4 value = {
+        imm.getImmediate<float>(0u), imm.getImmediate<float>(1u),
+        imm.getImmediate<float>(2u), imm.getImmediate<float>(3u)
+      };
+      m_immediateConstants.floats.push_back({ index, value });
+    } else if (op.getOpCode() == OpCode::eDefI) {
+      m_immediateConstants.intCount = std::max(m_immediateConstants.intCount, index + 1u);
+    } else if (op.getOpCode() == OpCode::eDefB) {
+      m_immediateConstants.boolCount = std::max(m_immediateConstants.boolCount, index + 1u);
+    } else {
+      return false;
+    }
+
+    return true;
+  }
+
+
+  bool D3D9ShaderAnalysis::HandleTextureSample(const Instruction& op) {
+    uint32_t samplerIndex;
+    auto dst = op.getDst();
+    Operand src1;
+    if (op.getSrcCount() >= 2u)
+      src1 = op.getSrc(1u);
+
+    switch (op.getOpCode()) {
+      case OpCode::eTexLd:
+        if (GetShaderInfo().getVersion().first <= 1u)
+          samplerIndex = dst.getIndex();
+        else
+          samplerIndex = src1.getIndex();
+        break;
+
+      case OpCode::eTexLdl:
+      case OpCode::eTexLdd:
+        samplerIndex = src1.getIndex();
+        break;
+
+      case OpCode::eTexReg2Ar:
+      case OpCode::eTexReg2Gb:
+      case OpCode::eTexReg2Rgb:
+      case OpCode::eTexM3x2Tex:
+      case OpCode::eTexM3x3Tex:
+      case OpCode::eTexM3x3:
+      case OpCode::eTexM3x2Depth:
+      case OpCode::eTexM3x3Spec:
+      case OpCode::eTexM3x3VSpec:
+      case OpCode::eTexDp3Tex:
+      case OpCode::eTexDp3:
+      case OpCode::eTexBem:
+      case OpCode::eTexBemL:
+        samplerIndex = dst.getIndex();
+        break;
+
+      default: return false;
+    }
+
+    m_usedSamplers |= 1u << samplerIndex;
+
+    return true;
+  }
+
+
+  bool D3D9ShaderAnalysis::HandleDcl(const Instruction& op) {
+    dxbc_spv_assert(op.hasDcl());
+    const auto& dcl = op.getDcl();
+    dxbc_spv_assert(op.hasDst());
+    const auto& dst = op.getDst();
+
+    RegisterType registerType = dst.getRegisterType();
+    uint32_t index = dst.getIndex();
+
+    if (registerType == RegisterType::eSampler) {
+      switch (dcl.getTextureType()) {
+        case TextureType::eTexture3D:
+          m_imageViewTypes[index] = VK_IMAGE_VIEW_TYPE_3D;
+          break;
+        case TextureType::eTextureCube:
+          m_imageViewTypes[index] = VK_IMAGE_VIEW_TYPE_CUBE;
+          break;
+        case TextureType::eTexture2D:
+        default:
+          m_imageViewTypes[index] = VK_IMAGE_VIEW_TYPE_2D;
+          break;
+      }
+      m_usedSamplers |= 1u << index;
+      return true;
+    }
+
+    if (registerType == RegisterType::eInput && GetShaderInfo().getType() == ShaderType::eVertex) {
+      m_inputSignature.push_back(Semantic { dcl.getSemanticUsage(), dcl.getSemanticIndex() });
+      return true;
+    }
+
+    return true;
+  }
+
+
+  D3D9ShaderAnalysis& D3D9ShaderAnalysis::operator=(const D3D9ShaderAnalysis& other) {
+    if (this == &other)
+      return *this;
+
+    m_isSWVP = other.m_isSWVP;
+    m_length = other.m_length;
+    m_shaderInfo = other.m_shaderInfo;
+    m_constants = other.m_constants;
+    m_immediateConstants = other.m_immediateConstants;
+    m_usedRTs = other.m_usedRTs;
+    m_usedSamplers = other.m_usedSamplers;
+    m_imageViewTypes = other.m_imageViewTypes;
+    m_inputSignature = other.m_inputSignature;
+
+    return *this;
+  }
+
+  D3D9ShaderAnalysis& D3D9ShaderAnalysis::operator=(D3D9ShaderAnalysis&& other) {
+    if (this == &other)
+      return *this;
+
+    m_isSWVP = other.m_isSWVP;
+    m_length = other.m_length;
+    m_shaderInfo = other.m_shaderInfo;
+    m_constants = other.m_constants;
+    m_immediateConstants = std::move(other.m_immediateConstants);
+    m_usedRTs = other.m_usedRTs;
+    m_usedSamplers = other.m_usedSamplers;
+    m_imageViewTypes = other.m_imageViewTypes;
+    m_inputSignature = std::move(other.m_inputSignature);
+    other.m_length = 0u;
+
+    return *this;
+  }
+
+}
---
+++ src/d3d9/d3d9_shader_analysis.h
@@ -0,0 +1,132 @@
+#pragma once
+
+#include "../util/util_vector.h"
+#include "../util/util_small_vector.h"
+
+#include <util/util_byte_stream.h>
+
+#include <sm3/sm3_types.h>
+#include <sm3/sm3_parser.h>
+
+#include <vulkan/vulkan.h>
+
+#include <vector>
+
+namespace dxvk {
+
+struct D3D9ImmediateFloatConstant {
+  uint32_t index;
+  Vector4 value;
+};
+
+using D3D9ImmediateFloatConstants = std::vector<D3D9ImmediateFloatConstant>;
+
+struct D3D9ImmediateConstants {
+  uint32_t floatCount = 0u;
+  uint32_t intCount   = 0u;
+  uint32_t boolCount  = 0u;
+
+  D3D9ImmediateFloatConstants floats;
+};
+
+struct D3D9ShaderConstantsInfo {
+  bool floatsAccessedDynamically = false;
+
+  uint32_t floatCount = 0u;
+  uint32_t intCount   = 0u;
+  uint32_t boolCount  = 0u;
+
+  uint32_t boolMask = 0u;
+};
+
+using D3D9RenderTargetMask = uint8_t;
+using D3D9SamplerMask      = uint32_t;
+using D3D9InputSignature   = small_vector<dxbc_spv::sm3::Semantic, 16u>;
+
+class D3D9ShaderAnalysis {
+
+public:
+
+  D3D9ShaderAnalysis() = default;
+
+  D3D9ShaderAnalysis(dxbc_spv::util::ByteReader code, bool isSWVP);
+
+  D3D9ShaderAnalysis(D3D9ShaderAnalysis&& other);
+
+  D3D9ShaderAnalysis(const D3D9ShaderAnalysis& other);
+
+  D3D9ShaderAnalysis& operator=(const D3D9ShaderAnalysis& other);
+
+  D3D9ShaderAnalysis& operator=(D3D9ShaderAnalysis&& other);
+
+  dxbc_spv::sm3::ShaderInfo GetShaderInfo() const {
+    return m_shaderInfo;
+  }
+
+  size_t GetLength() const {
+    return m_length;
+  }
+
+  const D3D9ShaderConstantsInfo& GetConstantsInfo() const {
+    return m_constants;
+  }
+
+  const D3D9ImmediateConstants& GetImmediateConstants() const {
+    return m_immediateConstants;
+  }
+
+  D3D9RenderTargetMask GetRenderTargetMask() const {
+    return m_usedRTs;
+  }
+
+  D3D9SamplerMask GetSamplerMask() const {
+    return m_usedSamplers;
+  }
+
+  VkImageViewType GetImageViewType(uint32_t index) const {
+    return m_imageViewTypes[index];
+  }
+
+  bool IsSWVP() const {
+    return m_isSWVP;
+  }
+
+  const D3D9InputSignature& GetInputSignature() const { return m_inputSignature; }
+
+  explicit operator bool() const {
+    return m_length != 0u;
+  }
+
+private:
+
+  bool RunAnalysis(dxbc_spv::sm3::Parser& parser);
+
+  bool HandleInstruction(const dxbc_spv::sm3::Instruction& op);
+
+  bool HandleDef(const dxbc_spv::sm3::Instruction& op);
+
+  bool HandleTextureSample(const dxbc_spv::sm3::Instruction& op);
+
+  bool HandleDcl(const dxbc_spv::sm3::Instruction& op);
+
+  bool m_isSWVP = false;
+
+  uint32_t m_length = 0u;
+
+  dxbc_spv::sm3::ShaderInfo m_shaderInfo;
+
+  D3D9ShaderConstantsInfo m_constants;
+
+  D3D9ImmediateConstants m_immediateConstants;
+
+  D3D9RenderTargetMask m_usedRTs = 0u;
+
+  D3D9SamplerMask m_usedSamplers = 0u;
+
+  std::array<VkImageViewType, 16u> m_imageViewTypes = {};
+
+  D3D9InputSignature m_inputSignature    = {};
+
+};
+
+}
