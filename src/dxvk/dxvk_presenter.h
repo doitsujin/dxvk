200cd0a06ced1db9cccd5754f8434f79aadde144
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2026-05-19 18:55:01 +0200

    [dxvk] Properly handle frame rate and present mode for present timing

--- src/dxvk/dxvk_presenter.h
+++ src/dxvk/dxvk_presenter.h
@@ -397,6 +397,8 @@ namespace dxvk {
     PresenterTimingInfo                     m_timingMode = { };
     uint32_t                                m_timingQueueSize = FrameQueueSize;
 
+    double                      m_frameRateLimit = 0.0;
+
     alignas(CACHE_LINE_SIZE)
     FpsLimiter                  m_fpsLimiter;
 
@@ -452,11 +454,11 @@ namespace dxvk {
 
     void updateDisplayTiming();
 
-    void updateTimingMode(VkPresentModeKHR presentMode);
+    void updateTimingMode();
 
     void recalibrateTimeDomains();
 
-    bool updatePresentTiming(VkPresentModeKHR presentMode);
+    bool updatePresentTiming();
 
     bool hasQpcDomain();
 
