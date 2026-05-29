3f498de0d8d1f33a2d478a21320ddd6fd7ec3b55
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2026-05-29 15:15:21 +0200

    [d3d9] Remove remaining legacy fixed-function code

--- src/d3d9/d3d9_fixed_function.h
+++ src/d3d9/d3d9_fixed_function.h
@@ -16,87 +16,8 @@
 namespace dxvk {
 
   class D3D9DeviceEx;
-  class SpirvModule;
 
   struct D3D9Options;
-  class D3D9ShaderSpecConstantManager;
-
-  struct D3D9FogContext {
-    // General inputs...
-    bool     IsPixel;
-    bool     RangeFog;
-    uint32_t RenderState;
-    uint32_t vPos;
-    uint32_t vFog;
-
-    uint32_t oColor;
-
-    bool     HasFogInput;
-
-    bool     IsFixedFunction;
-    bool     IsPositionT;
-    bool     HasSpecular;
-    uint32_t Specular;
-    uint32_t SpecUBO;
-  };
-
-  struct D3D9AlphaTestContext {
-    uint32_t alphaId;
-    uint32_t alphaPrecisionId;
-    uint32_t alphaFuncId;
-    uint32_t alphaRefId;
-  };
-
-  constexpr uint32_t GetGlobalSamplerSetIndex() {
-    // arbitrary, but must not conflict with bindings
-    return 15u;
-  }
-
-  constexpr uint32_t GetPushSamplerOffset(uint32_t samplerIndex) {
-    // Must not conflict with render state block
-    return MaxSharedPushDataSize + sizeof(uint16_t) * samplerIndex;
-  }
-
-  // Returns new oFog if VS
-  // Returns new oColor if PS
-  uint32_t DoFixedFunctionFog(D3D9ShaderSpecConstantManager& spec, SpirvModule& spvModule, const D3D9FogContext& fogCtx);
-
-  void DoFixedFunctionAlphaTest(SpirvModule& spvModule, const D3D9AlphaTestContext& ctx);
-
-  // Returns a render state block, as well as the index of the
-  // first sampler member.
-  std::pair<uint32_t, uint32_t> SetupRenderStateBlock(SpirvModule& spvModule, uint32_t samplerMask);
-
-  // Returns a global sampler descriptor array
-  uint32_t SetupSamplerArray(SpirvModule& spvModule);
-
-  // Common code to load a sampler from the sampler array
-  uint32_t LoadSampler(SpirvModule& spvModule, uint32_t descriptorId,
-    uint32_t pushBlockId, uint32_t pushMember, uint32_t samplerIndex);
-
-  struct D3D9PointSizeInfoVS {
-    uint32_t defaultValue;
-    uint32_t min;
-    uint32_t max;
-  };
-
-  // Default point size and point scale magic!
-  D3D9PointSizeInfoVS GetPointSizeInfoVS(D3D9ShaderSpecConstantManager& spec, SpirvModule& spvModule, uint32_t vPos, uint32_t vtx, uint32_t perVertPointSize, uint32_t rsBlock, uint32_t specUbo, bool isFixedFunction);
-
-  struct D3D9PointSizeInfoPS {
-    uint32_t isSprite;
-  };
-
-  D3D9PointSizeInfoPS GetPointSizeInfoPS(D3D9ShaderSpecConstantManager& spec, SpirvModule& spvModule, uint32_t rsBlock, uint32_t specUbo);
-
-  uint32_t GetPointCoord(SpirvModule& spvModule);
-
-  uint32_t GetSharedConstants(SpirvModule& spvModule);
-
-  uint32_t SetupSpecUBO(SpirvModule& spvModule, std::vector<DxvkBindingInfo>& bindings);
-
-  constexpr uint32_t TCIOffset = 16;
-  constexpr uint32_t TCIMask   = 0b111 << TCIOffset;
 
   enum D3D9FF_VertexBlendMode {
     D3D9FF_VertexBlendMode_Disabled,
@@ -104,7 +25,9 @@ namespace dxvk {
     D3D9FF_VertexBlendMode_Tween,
   };
 
-  constexpr uint32_t TextureArgCount = 3;
+  // No idea what this is
+  constexpr uint32_t TCIOffset = 16;
+  constexpr uint32_t TCIMask   = 0b111 << TCIOffset;
 
   class D3D9FFShaderModuleSet : public RcObject {
 
@@ -126,6 +49,11 @@ namespace dxvk {
 
     static Rc<DxvkShader> buildVs();
     static Rc<DxvkShader> buildFs(D3D9DeviceEx* pDevice);
+
+    constexpr static uint32_t GetPushSamplerOffset(uint32_t samplerIndex) {
+      // Must not conflict with render state block
+      return MaxSharedPushDataSize + sizeof(uint16_t) * samplerIndex;
+    }
 
   };
 
