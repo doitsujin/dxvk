9d0f0023aa04eb9475ca93c5a15ed34ce0e60f1f
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2026-06-07 16:54:20 +0200

    [d3d9] Add fast path for recursive device lock

--- src/d3d9/d3d9_multithread.h
+++ src/d3d9/d3d9_multithread.h
@@ -4,34 +4,27 @@
 
 namespace dxvk {
 
+  class D3D9Multithread;
+
   /**
-   * \brief Device lock
-   *
-   * Lightweight RAII wrapper that implements
-   * a subset of the functionality provided by
-   * \c std::unique_lock, with the goal of being
-   * cheaper to construct and destroy.
+   * \brief Scoped device lock
    */
   class D3D9DeviceLock {
 
   public:
 
-    D3D9DeviceLock()
-      : m_mutex(nullptr) { }
+    D3D9DeviceLock() { }
 
-    D3D9DeviceLock(sync::RecursiveSpinlock& mutex)
-      : m_mutex(&mutex) {
-      mutex.lock();
-    }
+    D3D9DeviceLock(D3D9Multithread& mutex)
+    : m_mutex(&mutex) { }
 
     D3D9DeviceLock(D3D9DeviceLock&& other)
-      : m_mutex(other.m_mutex) {
+    : m_mutex(other.m_mutex) {
       other.m_mutex = nullptr;
     }
 
     D3D9DeviceLock& operator = (D3D9DeviceLock&& other) {
-      if (m_mutex)
-        m_mutex->unlock();
+      Unlock();
 
       m_mutex = other.m_mutex;
       other.m_mutex = nullptr;
@@ -39,13 +32,14 @@ namespace dxvk {
     }
 
     ~D3D9DeviceLock() {
-      if (m_mutex != nullptr)
-        m_mutex->unlock();
+      Unlock();
     }
 
   private:
 
-    sync::RecursiveSpinlock* m_mutex;
+    D3D9Multithread* m_mutex = nullptr;
+
+    void Unlock();
 
   };
 
@@ -54,24 +48,54 @@ namespace dxvk {
    * \brief D3D9 context lock
    */
   class D3D9Multithread {
+    static constexpr uint32_t InvalidTid = -1u;
 
+    friend D3D9DeviceLock;
   public:
 
-    D3D9Multithread(
-      BOOL                  Protected);
+    D3D9Multithread(BOOL Protected)
+    : m_protected(Protected) { }
 
+    /**
+     * \brief Acquires lock
+     *
+     * If the calling thread already owns the lock, this will return
+     * an empty lock guard and rely entirely on proper scoping.
+     * \returns Lock guard
+     */
     D3D9DeviceLock AcquireLock() {
-      return m_protected
-        ? D3D9DeviceLock(m_mutex)
-        : D3D9DeviceLock();
+      if (likely(!m_protected))
+        return D3D9DeviceLock();
+
+      uint32_t expected = 0u;
+      uint32_t threadId = dxvk::this_thread::get_id();
+
+      if (likely(m_owner.compare_exchange_weak(expected, threadId, std::memory_order_acquire)))
+        return D3D9DeviceLock(*this);
+
+      if (expected == threadId)
+        return D3D9DeviceLock();
+
+      return LockContested(threadId);
     }
 
   private:
 
-    BOOL            m_protected;
+    alignas(CACHE_LINE_SIZE)
+    std::atomic<uint32_t>   m_owner     = { 0u };
+    BOOL                    m_protected = false;
 
-    sync::RecursiveSpinlock m_mutex;
+    D3D9DeviceLock LockContested(uint32_t threadId);
+
+    void Unlock() {
+      m_owner.store(0u, std::memory_order_release);
+    }
 
   };
+
+  inline void D3D9DeviceLock::Unlock() {
+    if (m_mutex)
+      m_mutex->Unlock();
+  }
 
 }
