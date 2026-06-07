c7b0e7bc31106d2976c5c1c6b5a505e540ba818b
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2026-06-07 16:54:20 +0200

    [d3d9] Add fast path for recursive device lock

--- src/d3d9/d3d9_multithread.cpp
+++ src/d3d9/d3d9_multithread.cpp
@@ -2,8 +2,14 @@
 
 namespace dxvk {
 
-  D3D9Multithread::D3D9Multithread(
-          BOOL                  Protected)
-    : m_protected( Protected ) { }
+  D3D9DeviceLock D3D9Multithread::LockContested(uint32_t threadId) {
+    // Spin until we can take ownership of the lock.
+    sync::spin(2000, [this, threadId] {
+      uint32_t expected = 0u;
+      return m_owner.compare_exchange_weak(expected, threadId, std::memory_order_acquire);
+    });
+
+    return D3D9DeviceLock(*this);
+  }
 
 }
