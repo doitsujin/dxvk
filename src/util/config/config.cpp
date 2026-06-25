6f279d4dcfe461f11cde9bfcd243e68c19611405
Blisto91 <47954800+Blisto91@users.noreply.github.com>
2026-06-25 21:17:20 +0200

    [util] Enable Strict floatEmulation for Gothic 3

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -532,9 +532,11 @@ namespace dxvk {
       { "d3d9.lenientClear",                "True" },
       { "d3d9.supportDFFormats",           "False" },
     }} },
-    /* Gothic 3                                   */
+    /* Gothic 3 - Rendering such as water broken  *
+     * on non strict float path with dxvk 3.0     */
     { R"(\\Gothic(3|3Final| III Forsaken Gods)\.exe$)", {{
       { "d3d9.supportDFFormats",           "False" },
+      { "d3d9.floatEmulation",            "Strict" },
     }} },
     /* Sonic Adventure 2                          */
     { R"(\\Sonic Adventure 2\\(launcher|sonic2app)\.exe$)", {{
