84c34155aa093a55d2e57032c93696cae0899515
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2026-06-28 15:29:13 +0200

    [dxvk] Adjust legacy descriptor pool allocation
    - We do need more than 16 storage buffers per pool given that compute
      pipelines can easily access.
    - Inline UBO is rare, but some drivers apparently require us to
      reserve storage per space rather than an arbitrary upper bound.

--- src/dxvk/dxvk_descriptor_pool.cpp
+++ src/dxvk/dxvk_descriptor_pool.cpp
@@ -142,14 +142,14 @@ namespace dxvk {
     constexpr static uint32_t MaxSets = 1024u;
 
     static const std::array<VkDescriptorPoolSize, 8> pools = {{
-      { VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE,          MaxSets / 2  },
-      { VK_DESCRIPTOR_TYPE_STORAGE_IMAGE,          MaxSets / 64 },
-      { VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER,   MaxSets / 2  },
-      { VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER,   MaxSets / 64 },
-      { VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,         MaxSets * 2  },
-      { VK_DESCRIPTOR_TYPE_STORAGE_BUFFER,         MaxSets / 2  },
-      { VK_DESCRIPTOR_TYPE_INPUT_ATTACHMENT,       MaxSets / 64 },
-      { VK_DESCRIPTOR_TYPE_INLINE_UNIFORM_BLOCK,   0x4000u      },
+      { VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE,          MaxSets / 2   },
+      { VK_DESCRIPTOR_TYPE_STORAGE_IMAGE,          MaxSets / 4   },
+      { VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER,   MaxSets / 2   },
+      { VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER,   MaxSets / 4   },
+      { VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,         MaxSets * 2   },
+      { VK_DESCRIPTOR_TYPE_STORAGE_BUFFER,         MaxSets / 2   },
+      { VK_DESCRIPTOR_TYPE_INPUT_ATTACHMENT,       MaxSets / 64  },
+      { VK_DESCRIPTOR_TYPE_INLINE_UNIFORM_BLOCK,   MaxSets * 128 },
     }};
 
     VkDescriptorPoolInlineUniformBlockCreateInfo inlineUboInfo = { VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_INLINE_UNIFORM_BLOCK_CREATE_INFO };
