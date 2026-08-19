8c4fd2723c031b425fb857454e9e5fa4ca25f4ac
Jacek Caban <jacek@codeweavers.com>
2026-08-20 00:56:20 +0200

    [util] Use hybrid_patchable attribute for DXVK_HOTPATCHABLE on ARM64EC

--- src/util/util_hotpatch.h
+++ src/util/util_hotpatch.h
@@ -6,6 +6,10 @@
 
 #if __has_attribute(ms_hook_prologue) && (defined(__i386__) || defined(__x86_64__))
 #define DXVK_HOTPATCHABLE __attribute__((__ms_hook_prologue__))
+#elif defined(DECLSPEC_CHPE_PATCHABLE)
+#define DXVK_HOTPATCHABLE DECLSPEC_CHPE_PATCHABLE
+#elif defined(__arm64ec__)
+#define DXVK_HOTPATCHABLE __attribute__((hybrid_patchable))
 #else
 #define DXVK_HOTPATCHABLE
 #endif
