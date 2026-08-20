375c522159bfef26d42203775eeb6d1c02f50287
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2026-08-20 16:32:54 +0200

    [dxvk] Fix dynamic multisample state not getting applied in some cases

--- src/dxvk/dxvk_context.cpp
+++ src/dxvk/dxvk_context.cpp
@@ -6794,26 +6794,26 @@ namespace dxvk {
       if (pipelineInfo.type == DxvkGraphicsPipelineType::BasePipeline) {
         // For pipelines created from graphics pipeline libraries, we need to
         // apply a bunch of dynamic state that is otherwise static or unused
-        if (!m_state.gp.flags.test(DxvkGraphicsPipelineFlag::HasRasterizerDiscard)) {
-          m_flags.set(DxvkContextFlag::GpDynamicDepthBias,
-                      DxvkContextFlag::GpDynamicDepthTest,
-                      DxvkContextFlag::GpDynamicStencilTest);
+        m_flags.set(DxvkContextFlag::GpDynamicDepthBias,
+                    DxvkContextFlag::GpDynamicDepthTest,
+                    DxvkContextFlag::GpDynamicStencilTest);
 
-          if (m_device->features().extExtendedDynamicState3.extendedDynamicState3DepthClipEnable)
-            m_flags.set(DxvkContextFlag::GpDynamicDepthClip);
+        if (m_device->features().extExtendedDynamicState3.extendedDynamicState3DepthClipEnable)
+          m_flags.set(DxvkContextFlag::GpDynamicDepthClip);
 
-          if (m_device->features().core.features.depthBounds)
-            m_flags.set(DxvkContextFlag::GpDynamicDepthBounds);
+        if (m_device->features().core.features.depthBounds)
+          m_flags.set(DxvkContextFlag::GpDynamicDepthBounds);
 
-          if (m_device->features().extExtendedDynamicState3.extendedDynamicState3RasterizationSamples
-          && m_device->features().extExtendedDynamicState3.extendedDynamicState3SampleMask
-          && m_state.gp.flags.test(DxvkGraphicsPipelineFlag::HasSampleRateShading))
-            m_flags.set(DxvkContextFlag::GpDynamicMultisampleState);
-
-          if (m_device->canUseSampleLocations(0u))
-            m_flags.set(DxvkContextFlag::GpDynamicSampleLocations);
+        if (m_device->features().extExtendedDynamicState3.extendedDynamicState3RasterizationSamples
+         && m_device->features().extExtendedDynamicState3.extendedDynamicState3SampleMask) {
+          m_flags.set(m_state.gp.flags.test(DxvkGraphicsPipelineFlag::HasSampleRateShading)
+            ? DxvkContextFlag::GpDynamicMultisampleState
+            : DxvkContextFlag::GpDirtyMultisampleState);
         }
-      } else if (!m_state.gp.flags.test(DxvkGraphicsPipelineFlag::HasRasterizerDiscard)) {
+
+        if (m_device->canUseSampleLocations(0u))
+          m_flags.set(DxvkContextFlag::GpDynamicSampleLocations);
+      } else {
         // Conditionally set up dynamic state based on pipeline state.
         // Must match DxvkGraphicsPipelineDynamicState behaviour exactly.
         if (m_device->features().core.features.depthBounds) {
@@ -6835,7 +6835,7 @@ namespace dxvk {
         m_flags.set(m_state.gp.state.useDynamicStencilTest()
           ? DxvkContextFlags(DxvkContextFlag::GpDynamicStencilTest)
           : DxvkContextFlags(DxvkContextFlag::GpDirtyStencilTest,
-                            DxvkContextFlag::GpDirtyStencilRef));
+                             DxvkContextFlag::GpDirtyStencilRef));
 
         // Dirty state that is never dynamic for optimized pipelines
         if (m_device->features().extExtendedDynamicState3.extendedDynamicState3DepthClipEnable)
