6c70fd8f34379b92eb18fe73fb8029a620f3a5c2
Justin Becker <jubecker@nvidia.com>
2026-08-20 16:07:17 -0700

    Annotate ResizeTarget(), ResizeBuffers1() as hybrid_patchable

--- src/dxgi/dxgi_swapchain.h
+++ src/dxgi/dxgi_swapchain.h
@@ -112,7 +112,8 @@ namespace dxvk {
             UINT                      Height,
             DXGI_FORMAT               NewFormat,
             UINT                      SwapChainFlags) final;
-    
+
+    DXVK_HOTPATCHABLE
     HRESULT STDMETHODCALLTYPE ResizeBuffers1(
             UINT                      BufferCount,
             UINT                      Width,
@@ -122,6 +123,7 @@ namespace dxvk {
       const UINT*                     pCreationNodeMask,
             IUnknown* const*          ppPresentQueue) final;
 
+    DXVK_HOTPATCHABLE
     HRESULT STDMETHODCALLTYPE ResizeTarget(
       const DXGI_MODE_DESC*           pNewTargetParameters) final;
     
